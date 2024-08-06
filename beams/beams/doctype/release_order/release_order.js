316d899de4491972d3dcbbf90dc9b6dd0a59f745
anjusha <anjushasuresh85@gmail.com>
2024-08-06 15:33:56 +0530

    fix release order functionality

--- beams/beams/doctype/release_order/release_order.js
+++ beams/beams/doctype/release_order/release_order.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Release Order", {
-// 	refresh(frm) {
+frappe.ui.form.on("Release Order", {
+  delivery_date: function (frm) {
+		$.each(frm.doc.items || [], function (i, d) {
+			if (!d.delivery_date) d.delivery_date = frm.doc.delivery_date;
+		});
+		refresh_field("items");
+	},
+});
 
-// 	},
-// });
+frappe.ui.form.on('Release Order Item', {
+    item_code: function(frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (row.item_code) {
+            row.qty = 1;
+            frm.refresh_field('items'); 
+        }
+    }
+});
--- beams/beams/doctype/release_order_item/release_order_item.py
+++ beams/beams/doctype/release_order_item/release_order_item.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2024, efeone and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
-class ReleaseOrder(Document):
+
+class ReleaseOrderItem(Document):
 	pass
