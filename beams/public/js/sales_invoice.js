a6b07d4d381dbc9f72abfcb6f125f14e62408457
hridyalakshmi <hridyalkrishnan2001@gmail.com>
2024-08-06 15:08:24 +0530

    feat: Enhance Sales Invoice form behavior based on customer

--- beams/public/js/sales_invoice.js
+++ beams/public/js/sales_invoice.js
@@ -1,5 +1,7 @@
 frappe.ui.form.on('Sales Invoice', {
+  // Function triggered on form load
     onload: function(frm) {
+      // Hide 'actual_customer' and 'actual_customer_group' fields by default
         frm.toggle_display('actual_customer', false);
         frm.toggle_display('actual_customer_group', false);
     },
@@ -23,6 +25,7 @@ frappe.ui.form.on('Sales Invoice', {
     },
     actual_customer: function(frm) {
         if (frm.doc.actual_customer) {
+          // Fetch the 'customer_group' of the selected 'actual_customer'
             frappe.db.get_value('Customer', frm.doc.actual_customer, 'customer_group', function(response) {
                 if (response && response.customer_group) {
                     frm.toggle_display('actual_customer_group', true);
