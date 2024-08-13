20686197bd584c56f15f8260f5c7d71516f1b7ec
anjusha <anjushasuresh85@gmail.com>
2024-08-13 14:05:55 +0530

    fix: Update logic for include_in_ibf field based on customer and is_barter_invoice status

--- beams/public/js/sales_invoice.js
+++ beams/public/js/sales_invoice.js
@@ -1,51 +1,60 @@
 frappe.ui.form.on('Sales Invoice', {
-  // Function triggered on form load
-    onload: function(frm) {
-      // Hide 'actual_customer' and 'actual_customer_group' fields by default
-        frm.toggle_display('actual_customer', false);
-        frm.toggle_display('actual_customer_group', false);
-        // Set a filter for 'actual_customer' to show only non-agent customers
-        frm.set_query('actual_customer', function() {
+  onload: function(frm) {
+    frm.toggle_display('actual_customer', false);
+    frm.toggle_display('actual_customer_group', false);
+    frm.trigger('customer');
+    // Set a filter for 'actual_customer' to show only non-agent customers
+    frm.set_query('actual_customer', function() {
             return {
                 filters: {
                     'is_agent': 0
                 }
             };
         });
-    },
-    customer: function(frm) {
-        if (frm.doc.customer) {
-            frappe.db.get_value('Customer', frm.doc.customer, ['is_agent'], function(value) {
-                if (value && value.is_agent) {
-                    frm.toggle_display('actual_customer', true);
-                    frm.set_value('actual_customer', '');
-                    frm.toggle_display('actual_customer_group', false);
-                } else {
-                    frm.toggle_display('actual_customer', false);
-                    frm.toggle_display('actual_customer_group', false);
-                }
-                frm.refresh_field('actual_customer');
-            });
+
+  },
+  customer: function(frm) {
+    if (frm.doc.customer) {
+      frappe.db.get_value('Customer', frm.doc.customer, ['is_agent'], function(value) {
+        if (value && value.is_agent) {
+          frm.toggle_display('actual_customer', true);
+          frm.set_value('actual_customer', '');
+          frm.toggle_display('actual_customer_group', false);
+
+          if (!frm.doc.is_barter_invoice) {
+            frm.set_value('include_in_ibf', 1);
+          }
         } else {
-            frm.toggle_display('actual_customer', false);
-            frm.toggle_display('actual_customer_group', false);
+          frm.toggle_display('actual_customer', false);
+          frm.toggle_display('actual_customer_group', false);
+          frm.set_value('include_in_ibf', 0);
         }
-    },
-    actual_customer: function(frm) {
-        if (frm.doc.actual_customer) {
-          // Fetch the 'customer_group' of the selected 'actual_customer'
-            frappe.db.get_value('Customer', frm.doc.actual_customer, 'customer_group', function(response) {
-                if (response && response.customer_group) {
-                    frm.toggle_display('actual_customer_group', true);
-                    frm.set_value('actual_customer_group', response.customer_group);
-                } else {
-                    frm.set_value('actual_customer_group', '');
-                    frm.toggle_display('actual_customer_group', false);
-                }
-            });
-        } else {
-            frm.set_value('actual_customer_group', '');
-            frm.toggle_display('actual_customer_group', false);
-        }
+        frm.refresh_field('actual_customer');
+      });
+    } else {
+      frm.toggle_display('actual_customer', false);
+      frm.toggle_display('actual_customer_group', false);
+      frm.set_value('include_in_ibf', 0);
     }
-    });
+  },
+  actual_customer: function(frm) {
+    if (frm.doc.actual_customer) {
+      frappe.db.get_value('Customer', frm.doc.actual_customer, 'customer_group', function(response) {
+        if (response && response.customer_group) {
+          frm.toggle_display('actual_customer_group', true);
+          frm.set_value('actual_customer_group', response.customer_group);
+        } else {
+          frm.set_value('actual_customer_group', '');
+          frm.toggle_display('actual_customer_group', false);
+        }
+      });
+    } else {
+      frm.set_value('actual_customer_group', '');
+      frm.toggle_display('actual_customer_group', false);
+    }
+  },
+  is_barter_invoice: function(frm) {
+    frm.set_value('include_in_ibf', 0);
+    frm.trigger('customer');
+  }
+});
