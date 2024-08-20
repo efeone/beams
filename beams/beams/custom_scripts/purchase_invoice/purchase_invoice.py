d7f7fd84b7dc2d7ace659aae5de9b5396e472c2f
Nidha Shirin <nidhashirin34@gmail.com>
2024-08-20 16:51:32 +0530

    chore : Adding Documentation

--- beams/beams/custom_scripts/purchase_invoice/purchase_invoice.py
+++ beams/beams/custom_scripts/purchase_invoice/purchase_invoice.py
@@ -1,11 +1,18 @@
 import frappe
 from frappe import _
+
+def before_save(doc, method):
+    """
+    Validate that the rounded total of the Purchase Invoice matches the linked Quotation before saving.
+    """
+    validate_purchase_invoice(doc, method)
+
 def validate_purchase_invoice(doc, method):
+    """
+    Checks if the rounded total of the Purchase Invoice matches the rounded total of the linked Quotation.
+    """
     if doc.quotation:  # assuming you have a field that links the Purchase Invoice to a Quotation
         quotation = frappe.get_doc('Quotation', doc.quotation)
 
         if doc.rounded_total != quotation.rounded_total:
-            frappe.throw(_("The rounded total amount of the Purchase Invoice does not match the Quotation amount. Please check and try again."))
-
-def before_save(doc, method):
-    validate_purchase_invoice(doc, method)
+            frappe.throw(_("The total amount of the Purchase Invoice does not match the Quotation amount. Please check and try again."))
