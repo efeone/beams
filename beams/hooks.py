990a61dad24fac4056145acca1c5389e78bbc920
anjusha <anjushasuresh85@gmail.com>
2024-08-21 12:56:09 +0530

    feat: Auto-assign ToDo to Account Users on Customer & Account creation

--- beams/hooks.py
+++ beams/hooks.py
@@ -134,6 +134,12 @@ doc_events = {
     },
     "Purchase Invoice": {
         "before_save": "beams.beams.custom_scripts.purchase_invoice.purchase_invoice.before_save"
+    },
+    "Account": {
+        "after_insert": "beams.beams.custom_scripts.account.account.create_todo_on_account_creation"
+    },
+    "Customer": {
+        "after_insert": "beams.beams.custom_scripts.customer.customer.create_todo_on_customer_creation"
     }
 }
 
