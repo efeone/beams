2caa2b09ad91894b2a1ac4a30938df67acd9c8ea
anjusha <anjushasuresh85@gmail.com>
2024-08-22 10:02:03 +0530

    feat: Purchase Order Workflow

--- beams/hooks.py
+++ beams/hooks.py
@@ -242,13 +242,13 @@ doc_events = {
 # }
 fixtures = [
     {"dt": "Workflow", "filters": [
-        ["name", "in", ["Customer Approval", "Account Approval", "Adhoc Budget","Supplier Approval"]]
-    ]},
+        ["name", "in", ["Customer Approval", "Account Approval", "Adhoc Budget","Supplier Approval", "Purchase Order Approval"]]
+    ]},    
     {"dt": "Workflow State", "filters": [
-        ["name", "in", ["Draft", "Pending Approval", "Approved", "Rejected", "Pending Finance Verification", "Verified By Finance","Rejected By Finance"]]
+        ["name", "in", ["Draft", "Pending Approval", "Approved", "Rejected", "Pending Finance Verification", "Verified By Finance","Rejected By Finance", "Pending Finance Approval", "Approved by Finance"]]
     ]},
     {"dt": "Workflow Action Master", "filters": [
-        ["name", "in", ["Submit for Approval", "Approve", "Reject", "Sent For Finance Verification", "Verify"]]
+        ["name", "in", ["Submit for Approval", "Approve", "Reject", "Sent For Finance Verification", "Verify", "Sent for Approval"]]
     ]},
     {"dt": "Role", "filters": [
         ["name", "in", ["CEO"]]
