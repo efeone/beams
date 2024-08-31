11f053976436ef807ae7204fa3c5201531ffce49
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2024-08-31 14:35:19 +0530

    fix:Added missing lines in hooks

--- beams/hooks.py
+++ beams/hooks.py
@@ -31,7 +31,9 @@ app_license = "mit"
 doctype_js = {
     "Sales Invoice": "public/js/sales_invoice.js",
     "Quotation": "public/js/quotation.js",
-    "Purchase Invoice": "public/js/purchase_invoice.js"
+    "Purchase Invoice": "public/js/purchase_invoice.js",
+    "Driver":"public/js/driver.js",
+    "Sales Order": "public/js/sales_order.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
