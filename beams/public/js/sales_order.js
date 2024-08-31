b7cdf008c446851a99d676f2cff480a47bc6c303
anjusha <anjushasuresh85@gmail.com>
2024-08-31 09:50:49 +0530

    chore: Apply documentation

--- beams/public/js/sales_order.js
+++ beams/public/js/sales_order.js
@@ -1,6 +1,8 @@
 frappe.ui.form.on('Sales Order', {
+  // Trigger the function when the 'sales_type' field changes
   sales_type: function(frm) {
       if (frm.doc.sales_type) {
+        // Set a custom query filter for the 'item_code' field in the 'items' child table
         frm.fields_dict['items'].grid.get_field('item_code').get_query = function(doc, cdt, cdn) {
           return {
             filters: {
