33094bf2fd30ba5b573f60f50aad6e06fdc25003
arathi <arathipk98@gmail.com>
2024-09-02 16:26:47 +0530

    feat:Filter and reset Quotation items by Sales Type

--- beams/public/js/quotation.js
+++ beams/public/js/quotation.js
@@ -36,6 +36,19 @@ frappe.ui.form.on('Quotation', {
 
     sales_type: function(frm) {
         check_sales_type(frm);
+
+        frm.clear_table('items');
+        frm.refresh_field('items');
+
+        if (frm.doc.sales_type) {
+            frm.fields_dict['items'].grid.get_field('item_code').get_query = function(doc, cdt, cdn) {
+                return {
+                    filters: {
+                        'sales_type': frm.doc.sales_type
+                    }
+                };
+            };
+        }
     }
 });
 
