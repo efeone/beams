f2f86834b51a3e3660fc6d42ac62251a0aa5f257
Neha Fathima <nehaputhur443@gmail.com>
2024-09-09 16:39:54 +0530

    fix: Added New Field Beams Accounts Settings and Filter the enabled print formats for Sales Invoice to Send Email from Sales Invoice

--- beams/hooks.py
+++ beams/hooks.py
@@ -130,7 +130,7 @@ before_uninstall = "beams.uninstall.before_uninstall"
 doc_events = {
     "Sales Invoice": {
         "before_save": "beams.beams.custom_scripts.sales_invoice.sales_invoice.validate_sales_invoice_amount_with_quotation",
-        "on_update_after_submit":"beams.beams.custom_scripts.sales_invoice.sales_invoice.send_email_to_party",
+        "on_update_after_submit":"beams.beams.custom_scripts.sales_invoice.sales_invoice.si_on_update_after_submit_custom",
         "autoname": "beams.beams.custom_scripts.sales_invoice.sales_invoice.autoname"
 
     },
