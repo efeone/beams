9e3c214db2b94451d8985e0364c7023481f39165
Nidha Shirin <nidhashirin34@gmail.com>
2024-09-10 11:54:33 +0530

    feat : Created doc-type Batta Claim Type

---
+++ beams/beams/doctype/batta_claim_type/batta_claim_type.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2024, efeone and contributors
+// For license information, please see license.txt
+frappe.ui.form.on('Accounts', {
+    company: function(frm, cdt, cdn) {
+        set_account_query(frm);
+    },
+    accounts_add: function(frm, cdt, cdn) {
+        set_account_query(frm);
+    }
+});
+
+// Set query to filter 'default_account' based on the selected company
+function set_account_query(frm) {
+    frm.fields_dict['accounts'].grid.get_field("default_account").get_query = function(doc, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        return {
+            filters: {'company': row.company}
+        };
+    };
+}
