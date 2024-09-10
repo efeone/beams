aa3f28c0ffb17b8bc64c2c103edcbe00afa5db2b
anjusha <anjushasuresh85@gmail.com>
2024-09-10 10:39:41 +0530

    feat: Create Purchase Invoice & Journal Entry on the Approval of Batta Claim

--- beams/beams/doctype/batta_claim/batta_claim.py
+++ beams/beams/doctype/batta_claim/batta_claim.py
@@ -1,9 +1,63 @@
 # Copyright (c) 2024, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class BattaClaim(Document):
-	pass
+    def on_submit(self):
+        if self.workflow_state == 'Approved':
+            if self.batta_type == 'External':
+                self.create_purchase_invoice_from_batta_claim()
+            elif self.batta_type == 'Internal':
+                self.create_journal_entry_from_batta_claim()
+
+    def create_purchase_invoice_from_batta_claim(self):
+        '''
+            Creation of Purchase Invoice on The Approval Of the Batta Claim.
+        '''
+        purchase_invoice = frappe.new_doc('Purchase Invoice')
+        purchase_invoice.supplier = self.supplier
+        purchase_invoice.posting_date = frappe.utils.nowdate()
+        purchase_invoice.due_date = frappe.utils.add_days(purchase_invoice.posting_date, 30)
+        beams_account_settings = frappe.get_doc('Beams Accounts Settings')
+        batta_claim_service_item = beams_account_settings.batta_claim_service_item
+        purchase_invoice.append('items', {
+            'item_code': batta_claim_service_item,
+            'rate': self.total_driver_batta,
+            'qty': 1
+        })
+
+        purchase_invoice.insert()
+        purchase_invoice.submit()
+
+    def create_journal_entry_from_batta_claim(self):
+        '''
+            Creation of Journal Entry on the Approval of the Batta Claim.
+        '''
+
+        journal_entry = frappe.new_doc('Journal Entry')
+        journal_entry.posting_date = frappe.utils.nowdate()
+        beams_account_settings = frappe.get_doc('Beams Accounts Settings')
+
+        batta_payable_account = beams_account_settings.batta_payable_account
+        batta_expense_account = beams_account_settings.batta_expense_account
+
+        journal_entry.append('accounts', {
+            'account': batta_payable_account,
+            'party_type': 'Employee',
+            'party': self.employee,
+            'debit_in_account_currency': 0,
+            'credit_in_account_currency': self.total_driver_batta,
+        })
+
+        journal_entry.append('accounts', {
+            'account': batta_expense_account,
+            'party_type': 'Employee',
+            'party': self.employee,
+            'debit_in_account_currency': self.total_driver_batta,
+            'credit_in_account_currency': 0,
+        })
+
+        journal_entry.insert()
+        journal_entry.submit()
