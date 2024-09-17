62a8ffb0053558624836e426d744896ade679654
anjusha <anjushasuresh85@gmail.com>
2024-09-17 12:45:00 +0530

    feat: Update Substitute Booking doctype

--- beams/beams/doctype/substitute_booking/substitute_booking.js
+++ beams/beams/doctype/substitute_booking/substitute_booking.js
@@ -1,8 +1,48 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Substitute Booking", {
-// 	refresh(frm) {
+frappe.ui.form.on("Substitute Booking", {
+	daily_wage: function(frm) {
+			calculate_total_wage(frm);
+	}
+});
 
-// 	},
-// });
+frappe.ui.form.on('Substitution Bill Date', {
+    date: function(frm, cdt, cdn) {
+        // Validate dates and update no_of_days when a date is entered or changed
+        validate_dates(frm);
+    }
+});
+
+function validate_dates(frm) {
+    let dates = frm.doc.substitution_bill_date.map(row => row.date).filter(date => date);
+
+    // Check for duplicate dates
+    let unique_dates = [...new Set(dates)];
+    if (unique_dates.length !== dates.length) {
+        frappe.msgprint({
+            title: __('Message'),
+            message: __('Dates should be unique.'),
+            indicator: 'red'
+        });
+        frm.refresh_field('substitution_bill_date');
+        return;
+    }
+
+    let unique_dates_count = unique_dates.length;
+    frm.set_value('no_of_days', unique_dates_count);
+
+    // Calculate the total wage after updating no_of_days
+    calculate_total_wage(frm);
+}
+
+function calculate_total_wage(frm) {
+    let no_of_days = frm.doc.no_of_days;
+    let daily_wage = frm.doc.daily_wage;
+
+    if (no_of_days && daily_wage) {
+        // Calculate total wage
+        let total_wage = no_of_days * daily_wage;
+        frm.set_value('total_wage', total_wage);
+    }
+}
--- beams/beams/doctype/substitute_booking/substitute_booking.py
+++ beams/beams/doctype/substitute_booking/substitute_booking.py
@@ -1,9 +1,55 @@
 # Copyright (c) 2024, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class SubstituteBooking(Document):
-	pass
+    def before_save(self):
+        self.calculate_no_of_days()
+        self.calculate_total_wage()
+
+        old_doc = self.get_doc_before_save()
+        if old_doc and old_doc.workflow_state != self.workflow_state and self.workflow_state == "Pending Approval":
+            self.check_employee_leave()
+
+    def calculate_no_of_days(self):
+        '''
+            Method to calculate no of days based on dates specified in the child table Substitution Bill Date.
+        '''
+        dates = [row.date for row in self.substitution_bill_date if row.date]
+
+        unique_dates = list(set(dates))
+
+        if len(unique_dates) != len(dates):
+            frappe.throw(_("Dates should be unique."))
+
+        self.no_of_days = len(unique_dates)
+
+    def calculate_total_wage(self):
+        '''
+            Method to calculate total wage based on daily wage and no of days.
+        '''
+        if self.no_of_days and self.daily_wage:
+            self.total_wage = self.no_of_days * self.daily_wage
+        else:
+            self.total_wage = 0
+
+    def check_employee_leave(self):
+        '''
+            Method to verify whether the employee is on leave for each specified date in the child table Substitution Bill Date.
+        '''
+        employee = self.substituting_for
+
+        if self.substitution_bill_date and employee:
+            for date_entry in self.substitution_bill_date:
+                leave_exists = frappe.db.exists('Leave Application', {
+                    'employee': employee,
+                    'status': 'Approved',
+                    'from_date': ('<=', date_entry.date),
+                    'to_date': ('>=', date_entry.date)
+                })
+
+                if not leave_exists:
+                    formatted_date = date_entry.date.strftime("%d/%m/%Y")
+                    frappe.throw(f"Employee {employee} is not on leave on {formatted_date}.")
