2a99b5dad93ee49ae09031b31a776819a7ba218b
Neha Fathima <nehaputhur443@gmail.com>
2024-09-19 12:12:29 +0530

    fix:updated the albatross settings doctype

--- beams/beams/doctype/albatross_settings/albatross_settings.js
+++ beams/beams/doctype/albatross_settings/albatross_settings.js
@@ -2,39 +2,34 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Albatross Settings', {
-   /*
-   *  Applies a filter on `albatross_service_item` where is_stock_item'checkbox is unchecked in item doctype.
-   * Applies a filter to the `albatross_service_item` field based on the fetched Sales Types where is_time_sales
-    checkbox is checked in item doctype.
-   */
+    /*
+     * Applies a filter on `albatross_service_item` where is_stock_item checkbox is unchecked in item doctype.
+     * Applies a filter to the `albatross_service_item` field based on the fetched Sales Types where is_time_sales
+     * checkbox is checked in item doctype.
+     */
     onload: function (frm) {
-        // Initialize the list for filtering
-        frm.albatross_service_item_sales_types = [];
+        let albatross_service_item_sales_types = [];
+
         // Fetch Sales Types where is_time_sales is checked
-        frappe.call({
-            method: 'frappe.client.get_list',
-            args: {
-                doctype: 'Sales Type',
-                filters: {
-                    'is_time_sales': 1
-                },
-                fields: ['name']
+        frappe.db.get_list('Sales Type', {
+            filters: {
+                'is_time_sales': 1
             },
-            callback: function (data) {
-                if (data.message) {
-                    frm.albatross_service_item_sales_types = data.message.map(sales_type => sales_type.name);
-                    // Set query for the albatross_service_item field
-                    frm.fields_dict['albatross_service_item'].get_query = function () {
-                        return {
-                            filters: {
-                                'is_stock_item': 0, // Filter for is_stock_item being unchecked
-                                'sales_type': ['in', frm.albatross_service_item_sales_types] // Filter based on sales types
-                            }
-                        };
+            fields: ['name']
+        }).then(data => {
+            if (data && data.length > 0) {
+                albatross_service_item_sales_types = data.map(sales_type => sales_type.name);
+                // Set query for the albatross_service_item field
+                frm.fields_dict['albatross_service_item'].get_query = function () {
+                    return {
+                        filters: {
+                            'is_stock_item': 0, // Filter for is_stock_item being unchecked
+                            'sales_type': ['in', albatross_service_item_sales_types] // Filter based on sales types
+                        }
                     };
-                    frm.refresh_field('albatross_service_item');
-                }
+                };
+                frm.refresh_field('albatross_service_item');
             }
-        });
+        })
     }
 });
