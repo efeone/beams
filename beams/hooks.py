c6f94d39926c84976ceb8bba073096a14ebc659e
Neha Fathima <nehaputhur443@gmail.com>
2024-09-20 13:06:41 +0530

    feat: Added custom status indicators to Purchase Invoice and Sales Invoice ListView

--- beams/hooks.py
+++ beams/hooks.py
@@ -36,7 +36,10 @@ doctype_js = {
     "Sales Order": "public/js/sales_order.js",
     "Voucher Entry": "public/js/voucher_entry.js"
 }
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+doctype_list_js = {
+    "Sales Invoice" : "public/js/sales_invoice_list.js",
+    "Purchase Invoice":"public/js/purchase_invoice_list.js"
+}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
