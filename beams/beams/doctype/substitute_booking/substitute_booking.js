3cc3b01cce7723558cd212d2e7c5f70bde4eed6a
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2024-09-21 11:15:40 +0530

    feat:Created custom button to view leave details in Substitute Booking doctype

--- beams/beams/doctype/substitute_booking/substitute_booking.js
+++ beams/beams/doctype/substitute_booking/substitute_booking.js
@@ -1,8 +1,19 @@
-// Copyright (c) 2024, efeone and contributors
-// For license information, please see license.txt
+//  Copyright (c) 2024, efeone and contributors
+//  For license information, please see license.txt
 
-// frappe.ui.form.on("Substitute Booking", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on('Substitute Booking', {
+    refresh: function(frm) {
+        // Add "Leave Application" button under "View"
+        frm.add_custom_button(__('Leave Application List'), function () {
+            const employee = frm.doc.substituting_for;
+            if (employee) {
+                // Navigate to the Leave Application doctype for the specified employee
+                frappe.set_route('Form', 'Leave Application', { employee: employee });
+                }
+            else {
+                frappe.msgprint(__('Please specify an employee in the "Substituting For" field.'));
+              }
+            }, __("View"));
+          }
+        });
