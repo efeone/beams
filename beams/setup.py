8ef682862cb715b4285edfd9cd58155ec04e1774
Neha Fathima <nehaputhur443@gmail.com>
2024-09-24 10:01:02 +0530

    fix: fixed the alignment of is_barter checkbox in Quotation doctype

--- beams/setup.py
+++ beams/setup.py
@@ -232,7 +232,7 @@ def get_quotation_custom_fields():
                 "fieldname": "albatross_details_section",
                 "fieldtype": "Section Break",
                 "label": "Albatross Details",
-                "insert_after": "sales_type"
+                "insert_after": "is_barter"
             },
             {
                 "fieldname": "albatross_ro_id",
