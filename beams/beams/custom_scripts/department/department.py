bee37c0ae3826b30f5bd6aa31a9bb1e1d7dbbf2d
Nidha Shirin <nidhashirin34@gmail.com>
2024-10-01 13:09:27 +0530

    Added missing documentation

--- beams/beams/custom_scripts/department/department.py
+++ beams/beams/custom_scripts/department/department.py
@@ -1,5 +1,8 @@
 import frappe
-
+    """
+    Fetches the user IDs of employees who belong to a specific department and have the 'Hod' role.
+    Returns a list of user IDs for filtering in the client-side code.
+    """
 @frappe.whitelist()
 def get_hod_users(department_name):
     users = frappe.db.sql("""
