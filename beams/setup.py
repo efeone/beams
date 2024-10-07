e8fc680652d576f16bc1e493610dcee8eaee8bb6
Ajmal Roshan <ajmalroshan2002@gmail.com>
2024-10-07 12:09:16 +0530

    feat: Added the multiselect Field and applied the filtering based on employee status

--- beams/setup.py
+++ beams/setup.py
@@ -966,3 +966,58 @@ def create_custom_roles(role_name):
             print(f"Role already exists: {role_name}")
 
     frappe.db.commit()
+
+ 
+ 
+def get_job_requisition_custom_fields():
+    '''
+    Custom fields that need to be added to the Job Requisition Doctype.
+    '''
+    return {
+        "Job Requisition": [
+            {
+                "fieldname": "request_for",
+                "label": "Request For",
+                "fieldtype": "Select",
+                "options": "\nEmployee Exit\nStaffing Plan\nUnplanned",
+                "insert_after": "naming_series"
+            },
+            {
+                "fieldname": "employee_left",
+                "label": "Employees Who Left",
+                "fieldtype": "Table MultiSelect",
+                "options": "Employees Left", 
+                "insert_after": "request_for",
+                "depends_on": "eval:doc.request_for == 'Employee Exit'"
+            },
+            {
+                "fieldname": "staffing_plan",
+                "label": "Staffing Plan",
+                "fieldtype": "Link",
+                "options": "Staffing Plan",
+                "insert_after": "employee_left",
+                "depends_on": "eval:doc.request_for == 'Staffing Plan'"
+            },
+            {
+                "fieldname": "requested_by",
+                "label": "Requested By",
+                "fieldtype": "Link",
+                "options": "Employee",
+                "insert_after": "staffing_plan",
+            },
+            {
+                "fieldname": "posting_date",
+                "label": "Posting Date",
+                "fieldtype": "Date",
+                "insert_after": "requested_by",
+                "read_only": 1
+            },
+            {
+                "fieldname": "status",
+                "label": "Status",
+                "fieldtype": "Select",
+                "options": "\nActive\nInactive\nLeft",
+                "insert_after": "posting_date",
+            }
+        ]
+   }
