a206319eb5abf80cde29fe84f5e9140be6f9dd22
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2024-10-08 14:36:57 +0530

    fix:Added missing code

--- beams/beams/doctype/substitute_booking/substitute_booking.js
+++ beams/beams/doctype/substitute_booking/substitute_booking.js
@@ -2,6 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Substitute Booking", {
+  daily_wage: function(frm) {
+      calculate_total_wage(frm);
+  },
+
+  onload: function(frm) {
+    if (frm.is_new() && !frm.doc.expense_account) {  // Check if it's a new form and the field is empty
+    // Fetch the default debit account from Beams Account Settings
+      frappe.db.get_single_value('Beams Accounts Settings', 'default_debit_account')
+        .then(default_account => {
+          if (default_account) {
+            frm.set_value('expense_account', default_account);
+          } else {
+            frappe.msgprint(__('Default Debit Account is not set in Beams Account Settings.'));
+                  }
+             });
+    }
+},
+
     refresh: function(frm) {
         // Add custom button to view leave applications under "View"
         frm.add_custom_button(__('Leave Application List'), function() {
@@ -59,6 +77,7 @@ frappe.ui.form.on("Substitute Booking", {
             });
         }, __("View"));
 
+
         // Check for payment button visibility
         if (!frm.is_new() && !frm.doc.is_paid && frm.doc.workflow_state === "Approved") {
             frm.add_custom_button(__('Make Payment'), function() {
@@ -72,7 +91,7 @@ frappe.ui.form.on("Substitute Booking", {
             });
         }
 
-        // Disable '+' icon in connections for creating journal entry manually
+        // disabled '+' icon in connections for creating journal entry manually
         if (frm.doc.status === 'Approved') {
             frm.fields_dict['connections'].grid.wrapper.find('.grid-add').hide();
         } else {
@@ -83,8 +102,8 @@ frappe.ui.form.on("Substitute Booking", {
     // Triggered when the 'substituting_for' field is changed
     substituting_for: function(frm) {
         if (!frm.is_dirty() && frm.doc.substituting_for) {
-            frm.page.add_action_item(__('Leave Application List'), function() {
-                const employee = frm.doc.substituting_for;
+            frm.add_custom_button(__('Leave Application List'), function() {
+              const employee = frm.doc.substituting_for;
                 if (employee) {
                     frappe.set_route('List', 'Leave Application', { employee: employee });
                 } else {
@@ -102,42 +121,48 @@ frappe.ui.form.on("Substitute Booking", {
     }
 });
 
+
+
 frappe.ui.form.on('Substitution Bill Date', {
-    date: function(frm, cdt, cdn) {
-        validate_dates(frm);
-    },
-    substitution_bill_date_remove: function(frm) {
-        validate_dates(frm);
-    }
+	date: function(frm, cdt, cdn) {
+		// Validate dates and update no_of_days when a date is entered or changed
+		validate_dates(frm);
+	},
+	substitution_bill_date_remove: function(frm) {
+			validate_dates(frm);
+	}
 });
 
 function validate_dates(frm) {
-    let dates = frm.doc.substitution_bill_date.map(row => row.date).filter(date => date); // Get valid dates
+	let dates = frm.doc.substitution_bill_date.map(row => row.date).filter(date => date);
 
-    let unique_dates = [...new Set(dates)]; // Ensure unique dates
-    if (unique_dates.length !== dates.length) {
-        frappe.msgprint({
-            title: __('Message'),
-            message: __('Dates should be unique.'),
-            indicator: 'red'
-        });
-        frm.refresh_field('substitution_bill_date');
-        return;
-    }
+	// Check for duplicate dates
+	let unique_dates = [...new Set(dates)];
+	if (unique_dates.length !== dates.length) {
+		frappe.msgprint({
+			title: __('Message'),
+			message: __('Dates should be unique.'),
+			indicator: 'red'
+		});
+		frm.refresh_field('substitution_bill_date');
+		return;
+	}
 
-    // Set the number of unique days
-    let unique_dates_count = unique_dates.length;
-    frm.set_value('no_of_days', unique_dates_count);
+	let unique_dates_count = unique_dates.length;
+	frm.set_value('no_of_days', unique_dates_count);
 
-    calculate_total_wage(frm);
+	// Calculate the total wage after updating no_of_days
+	calculate_total_wage(frm);
 }
 
-function calculate_total_wage(frm) {
-    let no_of_days = frm.doc.no_of_days;
-    let daily_wage = frm.doc.daily_wage;
 
-    if (no_of_days && daily_wage) {
-        let total_wage = no_of_days * daily_wage;
-        frm.set_value('total_wage', total_wage); // Update total wage based on number of days and daily wage
-    }
+function calculate_total_wage(frm) {
+  let no_of_days = frm.doc.no_of_days;
+  let daily_wage = frm.doc.daily_wage;
+
+if (no_of_days && daily_wage) {
+  // Calculate total wage
+  let total_wage = no_of_days * daily_wage;
+  frm.set_value('total_wage', total_wage);
+}
 }
