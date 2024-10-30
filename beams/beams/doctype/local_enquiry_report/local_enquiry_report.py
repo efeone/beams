61a38949f1f0e0a15b6fa4b6f086f727c46f6b73
anjusha <anjushasuresh85@gmail.com>
2024-10-30 14:44:38 +0530

    fix: Use fetched data directly to update status

--- beams/beams/doctype/local_enquiry_report/local_enquiry_report.py
+++ beams/beams/doctype/local_enquiry_report/local_enquiry_report.py
@@ -11,10 +11,10 @@ class LocalEnquiryReport(Document):
 
 @frappe.whitelist()
 def set_status_to_overdue():
-    """
+    '''
      This function updates the status of Local Enquiry Reports. It sets the status to 'Overdue'for reports where the expected completion date is today or earlier,
      the enquiry completion date is not set or is later than the expected completion date, and the current status is not already 'Overdue'.
-    """
+    '''
     today = getdate(frappe.utils.today())
 
     # Fetch Local Enquiry Reports with expected completion date on or before today and status not set to 'Overdue'
@@ -25,9 +25,8 @@ def set_status_to_overdue():
 
     if enquiries:
         for enquiry in enquiries:
-            doc = frappe.get_doc('Local Enquiry Report', enquiry.name)
             # Check if 'Enquiry Completion Date' is not set or is later than 'Expected Completion Date'
-            if not doc.enquiry_completion_date or getdate(doc.enquiry_completion_date) > getdate(doc.expected_completion_date):
+            if not enquiry.enquiry_completion_date or getdate(enquiry.enquiry_completion_date) > getdate(enquiry.expected_completion_date):
                 frappe.db.set_value('Local Enquiry Report', enquiry.name, 'status', 'Overdue')
 
         frappe.db.commit()
