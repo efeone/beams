ce0654cb9bbc0c08017d46f550072b36a5018df1
nidhashirinniyas <nidhashirin34@gmail.com>
2024-11-13 14:53:03 +0530

    fix :Button in event shown before save

--- beams/beams/custom_scripts/event/event.js
+++ beams/beams/custom_scripts/event/event.js
@@ -4,8 +4,9 @@ frappe.ui.form.on('Event', {
      * This button creates a new 'Training Request' document.
      */
     refresh: function(frm) {
-        if (frappe.user.has_role('HOD')) {
-            // Adds the custom button 'Training Request' in the 'Create' section
+        // Check if the document is saved (not new) and user has the 'HOD' role
+        if (!frm.is_new() && frappe.user.has_role('HOD')) {
+            // Add the 'Training Request' button in the 'Create' section
             frm.add_custom_button('Training Request', function() {
                 // Creates a new 'Training Request' document
                 frappe.new_doc('Training Request');
