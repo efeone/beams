abb57ab4872a3533774d1c209fe00f74f2323275
Sherin KR <sherinkrply@gmail.com>
2024-11-20 15:57:06 +0530

    feat: validate feilds in Job Application form

--- beams/www/job_portal/job.py
+++ beams/www/job_portal/job.py
@@ -7,10 +7,20 @@ def get_context(context):
 	if not frappe.db.exists('Job Opening', job_opening):
 		frappe.log_error(str(job_opening), 'Job Opening {0} not found')
 		frappe.throw(_("Sorry, the link you are following is broken."), frappe.PermissionError)
-	context.job = frappe.get_doc('Job Opening', job_opening)
+	opening_doc = frappe.get_doc('Job Opening', job_opening)
+	context.job = opening_doc
 	context.salary_range = get_salary_range(job_opening)
 	context.no_of_applications = get_job_applicant_count(job_opening)
-	context.education_qualification = ''
+
+    #Preparing coma seperated string for Prefered qualifications
+	qualifications = ''
+	for qualification in opening_doc.min_education_qual:
+		qualifications += '{}, '.format(qualification.qualification)
+
+    #Removing last 2 chars from the string. which is ',' for the last added qualification
+	if qualifications:
+		qualifications = qualifications[:-2]
+	context.educational_qualifications = qualifications
 	return {
 		"context": context
 	}
