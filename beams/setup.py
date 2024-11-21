40070d23ea646e4a22be35f54efe519a3cb8d3b1
Neha Fathima <nehaputhur443@gmail.com>
2024-11-21 15:07:28 +0530

    fix: Change the label on a field in the Job Opening

--- beams/setup.py
+++ beams/setup.py
@@ -1564,7 +1564,7 @@ def get_job_opening_custom_fields():
             {
                "fieldname": "min_education_qual",
                 "fieldtype": "Table MultiSelect",
-                "label": "Minimum Educational Qualification",
+                "label": "Preferred Educational Qualification",
                 'options':"Educational Qualifications",
                 "insert_after": "qualification_details"
             },
