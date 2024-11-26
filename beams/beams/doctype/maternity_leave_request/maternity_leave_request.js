437e3fa344f88f44a0a7d2a312f4d01095685a00
nidhashirinniyas <nidhashirin34@gmail.com>
2024-11-26 14:48:53 +0530

    feat : Create doctype Maternity Leave Request

---
+++ beams/beams/doctype/maternity_leave_request/maternity_leave_request.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2024, efeone and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Maternity Leave Request', {
+    setup: function (frm) {
+        frm.set_query('employee', function () {
+            return {
+                filters: {
+                    gender: 'Female'
+                }
+            };
+        });
+    },
+    birth_count: function(frm) {
+        // Check the value of birth_count and set no_of_days accordingly
+        if (frm.doc.birth_count === 1 || frm.doc.birth_count === 2) {
+            frm.set_value('no_of_days', 180);  // Set no_of_days to 180 if birth_count is 1 or 2
+        } else if (frm.doc.birth_count >= 3) {
+            frm.set_value('no_of_days', 90);   // Set no_of_days to 90 if birth_count is 3 or more
+        }
+    }
+});
---
+++ beams/beams/doctype/maternity_leave_request/maternity_leave_request.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2024, efeone and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.utils import add_days
+
+class MaternityLeaveRequest(Document):
+
+	def on_update(self):
+		# Check if the workflow state is "Approved"
+		if self.workflow_state == "Approved":
+			# Fetch the Maternity Leave Type from HR Settings
+			maternity_leave_type = frappe.db.get_single_value("Beams HR Settings", "maternity_leave_type")
+
+			# Check if the Leave Type matches the Maternity Leave Type
+			if self.leave_type == maternity_leave_type:
+				self.create_leave_allocation()
+
+	def create_leave_allocation(self):
+		# Calculate the To Date as 365 days from the From Date
+		to_date = add_days(self.from_date, 365)
+
+		# Create Leave Allocation
+		leave_allocation = frappe.get_doc({
+			"doctype": "Leave Allocation",
+			"employee": self.employee,
+			"leave_type": self.leave_type,
+			"from_date": self.from_date,
+			"to_date": to_date,
+			"new_leaves_allocated": self.no_of_days,  
+		})
+		leave_allocation.insert(ignore_permissions=True)
+		leave_allocation.submit()
+
+		frappe.msgprint(f"Leave Allocation created for {self.employee} ({self.leave_type}) from {self.from_date} to {to_date}.")
