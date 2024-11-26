eedf1478da349863d6d710c2f816a9e7fc29cd3d
ajmalroshan123 <ajmalroshan2002@gmail.com>
2024-11-26 09:41:50 +0530

    fix: added documentation and corected the message

--- beams/beams/custom_scripts/leave_allocation/leave_allocation.py
+++ beams/beams/custom_scripts/leave_allocation/leave_allocation.py
@@ -2,6 +2,12 @@ import frappe
 
 
 def validate(doc ,method):
+    """
+     Validation for Leave Allocation DocType
+
+    This function validates that the selected leave type in the Leave Allocation
+    DocType is permitted for the employee based on their gender. 
+    """
     employee_gender = frappe.db.get_value("Employee", doc.employee, "gender")
     if not employee_gender:
         frappe.throw(f"Gender not found for Employee {doc.employee}. Please ensure gender is set in the Employee record.")
@@ -14,5 +20,5 @@ def validate(doc ,method):
         }
     )
 
-    if not is_valid_mapping:
-        frappe.throw(f"The Selected Leave Type is not permitted for the {doc.employee}")  
+    if not is_valid_mapping: 
+        frappe.throw(f"The Selected Leave Type is not permitted for this Employee {doc.employee}")  
