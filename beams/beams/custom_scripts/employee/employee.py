9413a4bb4868d2307ebe5ee12909ad46db92b799
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2024-11-26 14:54:03 +0530

    fix:Added documentation

--- beams/beams/custom_scripts/employee/employee.py
+++ beams/beams/custom_scripts/employee/employee.py
@@ -38,33 +38,35 @@ def get_employee_name_for_user(user_id):
 
 @frappe.whitelist()
 def after_insert_employee(doc, method):
-    # Fetch Beams HR Settings
-    default_settings = frappe.get_doc('Beams HR Settings')
-    leave_policy = default_settings.default_leave_policy
-    leave_period = default_settings.leave_period
+    '''This function is triggered after an Employee record is created.
+       It fetches the default leave policy and leave period from the Beams HR Settings,
+    validates the configurations, and automatically creates and submits a Leave Policy Assignment.'''
 
+# Fetch default leave policy and leave period from Beams HR Settings using frappe.db.get_single_value
+    leave_policy = frappe.db.get_single_value('Beams HR Settings', 'default_leave_policy')
+    leave_period = frappe.db.get_single_value('Beams HR Settings', 'leave_period')
     if not leave_policy or not leave_period:
         frappe.throw(_("Default Leave Policy or Leave Period is not configured in Beams HR Settings."))
 
     leave_period_details = frappe.db.get_value(
-        "Leave Period",
+        'Leave Period',
         leave_period,
-        ["from_date", "to_date"],
+        ['from_date', 'to_date'],
         as_dict=True,
     )
 
     if not leave_period_details:
-        frappe.throw(_("Leave Period {0} does not exist.").format(leave_period))
+        frappe.throw(_('Leave Period {0} does not exist.').format(leave_period))
 
     # Create Leave Policy Assignment
     leave_policy_assignment = frappe.get_doc({
-        "doctype": "Leave Policy Assignment",
-        "employee": doc.name,
-        "leave_policy": leave_policy,
-        "leave_period": leave_period,
-        "assignment_based_on": "Leave Period",
-        "effective_from": leave_period_details.from_date,
-        "effective_to": leave_period_details.to_date,
+        'doctype': 'Leave Policy Assignment',
+        'employee': doc.name,
+        'leave_policy': leave_policy,
+        'leave_period': leave_period,
+        'assignment_based_on': 'Leave Period',
+        'effective_from': leave_period_details.from_date,
+        'effective_to': leave_period_details.to_date,
     })
 
     leave_policy_assignment.insert()
