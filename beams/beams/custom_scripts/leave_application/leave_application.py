23ed7a81ae64072f3ffaa1d83bee0e78f45ecd4e
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2024-11-27 11:19:00 +0530

    feat:Added fields in employee doctype

--- beams/beams/custom_scripts/leave_application/leave_application.py
+++ beams/beams/custom_scripts/leave_application/leave_application.py
@@ -10,27 +10,25 @@ def validate_leave_type(doc, method):
 def validate_leave_advance_days(from_date, leave_type):
     '''
     Validates the `from_date` for Leave based on the minimum advance days specified in the Leave Type.
+    If min_advance_days is 0 or not provided, no validation is performed.
     '''
     if not leave_type:
         frappe.throw(_("Leave Type is required."))
 
-    # Fetch the Leave Type document
     leave_type_doc = frappe.get_doc("Leave Type", leave_type)
-
-    # Get the minimum advance days for the leave type
     min_advance_days = leave_type_doc.min_advance_days or 0
 
-    # Get the current date and calculate the minimum allowed from_date
+    if min_advance_days == 0:
+        return
+
     current_date = nowdate()
     min_allowed_date = add_days(current_date, min_advance_days)
 
-    # Check if the provided from_date is before the minimum allowed date
     if from_date and from_date < min_allowed_date:
         frappe.throw(
             _("The From Date must be at least {0} days from today for the selected Leave Type.")
             .format(min_advance_days)
         )
-
 
 @frappe.whitelist()
 def validate_leave_application(doc, method):
