c016935b0984416fd528ebefd779e7c3b64e1a7a
ajmalroshan123 <ajmalroshan2002@gmail.com>
2024-12-03 14:33:13 +0530

    feat: removed and created new button to create employee

---
+++ beams/beams/custom_scripts/job_offer/job_offer.js
@@ -0,0 +1,32 @@
+frappe.ui.form.on("Job Offer", {
+    refresh: function(frm) {
+        if (
+			!frm.doc.__islocal &&
+			frm.doc.status == "Accepted" &&
+			frm.doc.docstatus === 1 &&
+			(!frm.doc.__onload || !frm.doc.__onload.employee)
+		) {
+			frm.remove_custom_button(__("Create Employee"), );
+		}
+        setTimeout(1000);
+
+        if (
+			!frm.doc.__islocal &&
+			frm.doc.status == "Accepted" &&
+			frm.doc.docstatus === 1 &&
+			(!frm.doc.__onload || !frm.doc.__onload.employee)
+		) {
+			frm.add_custom_button(__("Create Employee "),function() {
+                make_employee(frm);
+            } );
+
+		}
+    }
+})
+
+function make_employee(frm) {
+    frappe.model.open_mapped_doc({
+        method: "beams.beams.custom_scripts.job_offer.job_offer.make_employee",
+        frm: frm,
+    });
+}
---
+++ beams/beams/custom_scripts/job_offer/job_offer.py
@@ -0,0 +1,42 @@
+import frappe
+from frappe.model.mapper import get_mapped_doc
+
+@frappe.whitelist()
+def make_employee(source_name, target_doc=None):
+    def set_missing_values(source, target):
+        # Fetch personal email and applicant name from Job Applicant
+        target.personal_email, target.first_name = frappe.db.get_value(
+            "Job Applicant", source.job_applicant, ["email_id", "applicant_name"]
+        )
+
+    # Map fields from Job Offer to Employee
+    doc = get_mapped_doc(
+        "Job Offer",
+        source_name,
+        {
+            "Job Offer": {
+                "doctype": "Employee",
+                "field_map": {
+                    "applicant_name": "employee_name",
+                    "offer_date": "scheduled_confirmation_date",
+                },
+            }
+        },
+        target_doc,
+        set_missing_values,
+    )
+
+    # Map additional fields from Job Applicant to Employee
+    job_applicant = frappe.get_doc("Job Offer", source_name).job_applicant
+    if job_applicant:
+        applicant_data          = frappe.get_doc("Job Applicant", job_applicant)
+        doc.gender              = applicant_data.gender
+        doc.date_of_birth       = applicant_data.date_of_birth
+        doc.cell_number         = applicant_data.current_mobile_no
+        doc.name_of_father      = applicant_data.father_name
+        doc.department          = applicant_data.department
+        doc.designation         = applicant_data.designation
+        doc.marital_status      = applicant_data.marital_status
+        doc.permanent_address   = applicant_data.permanent_address
+
+    return doc
