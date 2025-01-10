1ade10743efa4a806eecf20de3aaa4554ec17cf1
anjusha <anjushasuresh85@gmail.com>
2025-01-10 10:58:23 +0530

    Renamed Category DocType to Appraisal Category and updated related files

--- beams/beams/doctype/appraisal_category/appraisal_category.js
+++ beams/beams/doctype/appraisal_category/appraisal_category.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Category", {
+frappe.ui.form.on("Appraisal Category", {
   validate: function(frm) {
       if (frm.doc.appraisal_threshold < 0 || frm.doc.appraisal_threshold > 5) {
           frappe.throw(__('Appraisal Threshold must be between 0 and 5.'));
--- beams/beams/doctype/appraisal_category/appraisal_category.py
+++ beams/beams/doctype/appraisal_category/appraisal_category.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Category(Document):
+class AppraisalCategory(Document):
 	pass
