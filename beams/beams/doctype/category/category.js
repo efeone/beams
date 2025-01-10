f969c2084961d66f75e5fc850ce42c5c104c3fe9
anjusha <anjushasuresh85@gmail.com>
2025-01-10 10:42:46 +0530

    feat: Customize Category doctype

--- beams/beams/doctype/category/category.js
+++ beams/beams/doctype/category/category.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Category", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Category", {
+  validate: function(frm) {
+      if (frm.doc.appraisal_threshold < 0 || frm.doc.appraisal_threshold > 5) {
+          frappe.throw(__('Appraisal Threshold must be between 0 and 5.'));
+      }
+  }
+});
