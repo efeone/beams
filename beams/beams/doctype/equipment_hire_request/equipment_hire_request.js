aabf9c4c49ba31b633c5cb3295495ec0fc0b3b89
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2025-01-18 14:25:44 +0530

    feat:Added naming series and Added custom button Create Purchase Invoice

--- beams/beams/doctype/equipment_hire_request/equipment_hire_request.js
+++ beams/beams/doctype/equipment_hire_request/equipment_hire_request.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Equipment Hire Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Equipment Hire Request", {
+    refresh(frm) {
+      frm.add_custom_button(__('Purchase Invoice'), function () {
+          var invoice = frappe.model.get_new_doc("Purchase Invoice");
+          invoice.posting_date = frm.doc.posting_date;
+          frappe.set_route("form", "Purchase Invoice", invoice.name);
+      }, __("Create"));
+    },
+});
