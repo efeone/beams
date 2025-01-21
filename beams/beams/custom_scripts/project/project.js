b069ac501d00035dfcb375bf7df3050c78734465
RASNA96 <rasnaravindran654@gmail.com>
2025-01-21 09:24:26 +0530

    feat :  Add functionality to create Transportation Request from Project

--- beams/beams/custom_scripts/project/project.js
+++ beams/beams/custom_scripts/project/project.js
@@ -7,5 +7,12 @@ frappe.ui.form.on('Project', {
                 frm: frm,
             });
         }, __("Create"));
+        // Adds a button to the 'Project' form to create an Transportation Request.
+        frm.add_custom_button(__('Transportation Request'), function () {
+            frappe.model.open_mapped_doc({
+                method: "beams.beams.custom_scripts.project.project.create_transportation_request",
+                frm: frm,
+            });
+        }, __("Create"));
     }
 });
