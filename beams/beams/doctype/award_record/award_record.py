2540c7f54aba517043a776593d80ee34ed6caa7b
Avishna Murali <avishnachinnu26@gmail.com>
2025-01-22 16:41:43 +0530

    feat:Sum up the Amount from the child table 'expenses'

--- beams/beams/doctype/award_record/award_record.py
+++ beams/beams/doctype/award_record/award_record.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2025, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class AwardRecord(Document):
-	pass
+    def validate(self):
+        self.update_total_amount()
+
+    @frappe.whitelist()
+    def update_total_amount(self):
+        # Initialize total amount
+        total_amount = 0
+
+        # Sum up the Amount from the child table 'expenses'
+        if self.expenses:
+            for expense in self.expenses:
+                total_amount += expense.amount
+
+        # Set the Total Amount field
+        self.total_amount = total_amount
