a3192af379dc4e6bf7684e1f8db96163df9a34f1
NaseeraVP <naseeravp38@gmail.com>
2025-01-24 13:22:37 +0530

    Chore:Corrected the syntax error

--- beams/beams/doctype/inward_register/inward_register.py
+++ beams/beams/doctype/inward_register/inward_register.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2025, efeone and contributors
 # For license information, please see license.txt
 
+# inward_register.py
+
 import frappe
 from frappe.model.document import Document
 
-class FuelCard(Document):
-    pass  
+class InwardRegister(Document):
+    pass
