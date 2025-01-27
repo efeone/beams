eb2dc6d238611d90eee36e99d86c14500e72d9a5
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2025-01-27 12:19:59 +0530

    feat:Created doctype Guest Appointment

--- beams/beams/doctype/guest_appointment/guest_appointment.js
+++ beams/beams/doctype/guest_appointment/guest_appointment.js
@@ -10,5 +10,24 @@ frappe.ui.form.on('Guest Appointment', {
                 guest_appointment: frm.doc.name  // Optionally link the Event to this Guest Appointment
             });
         }, 'Create');  // Add the button under the 'Create' group
-    }
+
+      if (frm.doc.workflow_state === "Approved") {
+      // Add a button for 'Inward Register' only when workflow state is 'Approved'
+      frm.add_custom_button(__('Inward Register'), function() {
+        // Call a custom server-side method to create the Inward Register
+        frappe.call({
+            method: 'beams.beams.doctype.guest_appointment.guest_appointment.create_inward_register',
+            args: {
+                guest_appointment: frm.doc.name
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frappe.set_route('Form', 'Inward Register', r.message.name);
+                }
+            }
+        });
+    }, __("Create"));
+}
+
+}
 });
--- beams/beams/doctype/guest_appointment/guest_appointment.py
+++ beams/beams/doctype/guest_appointment/guest_appointment.py
@@ -1,35 +1,84 @@
-# Copyright (c) 2025, efeone and contributors
+# Copyright (c) 2024, efeone and contributors
 # For license information, please see license.txt
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import today
-
+from frappe.desk.form.assign_to import add as add_assign
+from frappe.utils import today, add_days
 
 class GuestAppointment(Document):
-	def on_submit(self):
-		self.validate_employee_availability()
+    def on_update_after_submit(self):
+        self.validate_employee_availability()
+        if self.workflow_state == "Approved":
+            self.send_appointment_notifications()
 
-	def validate_employee_availability(self):
-	    '''
-	    Validates if the employee associated with the document is available
-	    on the specified appointment date.
-		'''
+    def validate_employee_availability(self):
+        '''
+        Validates if the employee associated with the document is available
+        on the specified appointment date.
+        '''
+        if not self.received_by or not self.appointment_date:
+            frappe.throw("Received By and Appointment Date are mandatory.")
 
-	    if not self.received_by or not self.appointment_date:
-	        frappe.throw("Recieved By and Appointment Date are mandatory.")
+        leave_applications = frappe.db.exists(
+            "Leave Application",
+            {
+                "employee": self.received_by,
+                "status": "Approved",
+                "from_date": ["<=", self.appointment_date],
+                "to_date": [">=", self.appointment_date],
+            }
+        )
 
-	    leave_applications = frappe.db.exists(
-	        "Leave Application",
-	        {
-	            "employee": self.received_by,
-	            "status": "Approved",
-	            "from_date": ["<=", self.appointment_date],
-	            "to_date": [">=", self.appointment_date],
-	        }
-	    )
+        if leave_applications:
+            frappe.throw(
+                f"The employee {self.received_by} has an approved leave on {self.appointment_date}."
+            )
+    def send_appointment_notifications(self):
+        '''
+        Creates a ToDo  to the employee (received_by) when the workflow state is 'Approved'.
+        '''
+        if not self.received_by:
+            return
 
-	    if leave_applications:
-	        frappe.throw(
-	            f"The employee {self.received_by} has an approved leave on {self.appointment_date}."
-	        )
+        # Get the user ID and name of the employee
+        employee_user = frappe.get_value("Employee", self.received_by, "user_id")
+        employee_name = frappe.get_value("Employee", self.received_by, "employee_name")
+        if not employee_user:
+            return
+
+        # ToDo message
+        todo_message = f"An appointment with guest {self.received_by} has been scheduled on {self.appointment_date} at {self.appointment_time}."
+
+        # Check if a ToDo already exists for the same document and message
+        existing_todo = frappe.get_all(
+            "ToDo",
+            filters={
+                "reference_type": self.doctype,
+                "reference_name": self.name,
+                "status": "Open",
+                "description": todo_message,
+            },
+        )
+
+        if existing_todo:
+            return
+
+        add_assign({
+            "assign_to": [employee_user],
+            "doctype": self.doctype,
+            "name": self.name,
+            "description": todo_message,
+        })
+        
+@frappe.whitelist()
+def create_inward_register(guest_appointment):
+    appointment = frappe.get_doc("Guest Appointment", guest_appointment)
+
+    inward_register = frappe.new_doc("Inward Register")
+    inward_register.vistor_name = appointment.guest_name
+    inward_register.received_by = appointment.received_by
+    inward_register.purpose_of_visit = appointment.purpose_of_visit
+    inward_register.insert(ignore_mandatory=True)
+
+    return inward_register.name
