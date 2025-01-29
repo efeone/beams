7c548327c9ec864b225be444702dd4eb251b09fb
nidhashirinniyas <nidhashirin34@gmail.com>
2025-01-29 10:04:06 +0530

    chore : Renamed the connection names

--- beams/beams/custom_scripts/project_dashboard/project_dashboard.py
+++ beams/beams/custom_scripts/project_dashboard/project_dashboard.py
@@ -14,7 +14,8 @@ def get_data(data=None):
 			{"label": _("Material"), "items": ["Material Request", "BOM", "Stock Entry"]},
 			{"label": _("Sales"), "items": ["Sales Order", "Delivery Note", "Sales Invoice"]},
 			{"label": _("Purchase"), "items": ["Purchase Order", "Purchase Receipt", "Purchase Invoice"]},
-			{"label": _("Request"), "items": ["Adhoc Budget", "Equipment Hire Request", "Equipment Request", "Transportation Request", "Technical Request"]},
+			{"label": _("Budgets"), "items": ["Budget", "Adhoc Budget"]},
+			{"label": _("Programs"), "items": ["Equipment Hire Request", "Equipment Request", "Transportation Request", "Technical Request"]},
 
 		],
 	}
