37d17b8f0a81fbf80998510c2923c51634ccb3d2
Nishana N K <nishana8153@gmail.com>
2025-01-31 09:33:35 +0530

    feat: Remove Purchase Invoice Button from Vehicle Hire Request Button

--- beams/beams/doctype/vehicle_hire_request/vehicle_hire_request.js
+++ beams/beams/doctype/vehicle_hire_request/vehicle_hire_request.js
@@ -1,11 +1,8 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Vehicle Hire Request", {
-	refresh(frm) {
-    frm.add_custom_button(__('Purchase Invoice'), function (){
+// frappe.ui.form.on("Vehicle Hire Request", {
+// 	refresh(frm) {
 
-    }, __("Create"));
-
-	},
-});
+// 	},
+// });
