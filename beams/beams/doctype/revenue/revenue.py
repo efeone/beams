c321d164e3a9ff8a805309beb1f6b25e92c8671f
hridyalakshmi <hridyalkrishnan2001@gmail.com>
2025-02-01 12:09:02 +0530

    fix: Changed naming series of Revenue Doctype

--- beams/beams/doctype/revenue/revenue.py
+++ beams/beams/doctype/revenue/revenue.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2025, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Revenue(Document):
-	pass
+	def before_naming(self):
+		self.naming_series = f"{{{frappe.scrub(self.revenue_against)}}}./.{self.fiscal_year}/.###"
