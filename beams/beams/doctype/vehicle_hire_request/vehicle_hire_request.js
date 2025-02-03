3afd78337b93715ffa083382d00692eb8c1cec25
Nishana N K <nishana8153@gmail.com>
2025-02-03 15:45:04 +0530

    fix: Posting Date Validation

--- beams/beams/doctype/vehicle_hire_request/vehicle_hire_request.js
+++ beams/beams/doctype/vehicle_hire_request/vehicle_hire_request.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Vehicle Hire Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Vehicle Hire Request", {
+    posting_date(frm) {
+        frm.call({ method: "validate_posting_date", doc: frm.doc });
+    }
+});
