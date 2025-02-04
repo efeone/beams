7f47bac08a886a1ad4fd81cdf6b384ea74d447bd
hridyalakshmi <hridyalkrishnan2001@gmail.com>
2025-02-04 15:46:13 +0530

    fix: Update Division Naming Series

--- beams/beams/doctype/division/division.py
+++ beams/beams/doctype/division/division.py
@@ -4,18 +4,34 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-
+from frappe.model.naming import make_autoname
 
 class Division(Document):
     def validate(self):
         if self.cost_center:
-            divisions = frappe.get_list('Division', filters={'cost_center': self.cost_center}, fields=['name'])
+            divisions = frappe.get_list(
+                'Division',
+                filters={
+                    'cost_center': self.cost_center,
+                    'name': ['!=', self.name]
+                },
+                fields=['name']
+            )
 
             if divisions:
                 division_name = divisions[0].get('name')
                 frappe.throw(
                     _("The selected Cost Center is already assigned to Division: {0}. Please choose a different one.").format(division_name)
                 )
+
+    def before_rename(self, old_name, new_name, merge=False):
+        """
+        Ensure the division field is updated when the naming series changes.
+        """
+        if not merge:
+            division_name = new_name.split("-")[0]
+            self.division = division_name
+            frappe.db.set_value("Division", self.name, "division", division_name)
 
 @frappe.whitelist()
 def get_used_cost_centers():
