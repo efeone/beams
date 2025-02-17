fe995f9e18a304420799d9a6fb857c68d7a84789
Neha Fathima <nehaputhur443@gmail.com>
2025-02-17 13:25:11 +0530

    fixed teh intendation in code

--- beams/beams/doctype/asset_bundle/asset_bundle.js
+++ beams/beams/doctype/asset_bundle/asset_bundle.js
@@ -3,18 +3,18 @@
 
 frappe.ui.form.on("Asset Bundle", {
 	refresh:function(frm) {
-    frm.fields_dict['stock_items'].grid.get_field('item').get_query = function(doc, cdt, cdn) {
-      return {
-          filters: {
-              is_fixed_asset: 0
-          }
-      };
-  };
-},
-validate: function(frm) {
-    if (!frm.doc.stock_items?.length && !frm.doc.assets?.length && !frm.doc.bundles?.length) {
-        frappe.msgprint(__('At least one of Stock Items, Assets, or Bundles must be filled in.'));
-        frappe.validated = false;
-    }
-}
+		frm.fields_dict['stock_items'].grid.get_field('item').get_query = function(doc, cdt, cdn) {
+			return {
+				filters: {
+					is_fixed_asset: 0
+				}
+			};
+		};
+	},
+	validate: function(frm) {
+		if (!frm.doc.stock_items?.length && !frm.doc.assets?.length && !frm.doc.bundles?.length) {
+			frappe.msgprint(__('At least one of Stock Items, Assets, or Bundles must be filled in.'));
+			frappe.validated = false;
+		}
+	}
 });
