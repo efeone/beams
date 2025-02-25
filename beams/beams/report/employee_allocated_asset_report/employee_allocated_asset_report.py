44c4cd0355d23c0bedd1510595125803c36fd79f
Avishna Murali <avishnachinnu26@gmail.com>
2025-02-25 16:36:50 +0530

    feat:Add Item Code and Item Name in the report

--- beams/beams/report/employee_allocated_asset_report/employee_allocated_asset_report.py
+++ beams/beams/report/employee_allocated_asset_report/employee_allocated_asset_report.py
@@ -12,6 +12,8 @@ def get_columns():
     return [
         {"label": "Employee", "fieldname": "employee", "fieldtype": "Link", "options": "Employee", "width": 200},
         {"label": "Asset", "fieldname": "asset", "fieldtype": "Link", "options": "Asset", "width": 200},
+        {"label": "Item Code", "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 150},
+        {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 200},
         {"label": "Location", "fieldname": "location", "fieldtype": "Data", "width": 200},
         {"label": "Total Asset Cost", "fieldname": "total_asset_cost", "fieldtype": "Currency", "width": 150}
     ]
@@ -21,11 +23,18 @@ def get_data(filters):
     asset_filters = {}
 
     if filters.get("employee"):
-        asset_filters["custodian"] = filters["employee"]  
+        asset_filters["custodian"] = filters["employee"]
 
     assets = frappe.get_all(
         "Asset",
-        fields=["custodian as employee", "name as asset", "location", "total_asset_cost"],
+        fields=[
+            "custodian as employee",
+            "name as asset",
+            "item_code",
+            "item_name",
+            "location",
+            "total_asset_cost"
+        ],
         filters=asset_filters if asset_filters else None
     )
 
@@ -33,6 +42,8 @@ def get_data(filters):
         row = {
             "employee": asset["employee"],
             "asset": asset["asset"],
+            "item_code": asset.get("item_code"),
+            "item_name": asset.get("item_name"),
             "location": asset["location"],
             "total_asset_cost": asset["total_asset_cost"]
         }
