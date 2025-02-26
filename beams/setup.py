d6f173d610da6dcdda083a256daccbd6a6dc0ac6
NaseeraVP <naseeravp38@gmail.com>
2025-02-26 14:50:40 +0530

    feat:Created Role Technical Store Head through Setup.py

--- beams/setup.py
+++ beams/setup.py
@@ -3737,7 +3737,7 @@ def get_beams_roles():
     '''
         Method to get BEAMS specific roles
     '''
-    return ['Production Manager', 'CEO', 'Company Secretary', 'HOD','Enquiry Officer','Enquiry Manager','Shift Publisher','Program Producer','Operations Head','Operations User','Admin','Driver','Budget User']
+    return ['Production Manager', 'CEO', 'Company Secretary', 'HOD','Enquiry Officer','Enquiry Manager','Shift Publisher','Program Producer','Operations Head','Operations User','Admin','Driver','Budget User','Technical Store Head']
 
 def get_custom_translations():
     '''
