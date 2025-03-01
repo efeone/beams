516b98f27d6433c26a9f98bb36a6958367c25bb6
anjusha <anjushasuresh85@gmail.com>
2025-03-01 16:33:08 +0530

    fix: Rearrange fields in Job Requisition

--- beams/setup.py
+++ beams/setup.py
@@ -1643,13 +1643,6 @@ def get_job_requisition_custom_fields():
     '''
     return {
         "Job Requisition": [
-
-            {
-                "fieldname": "suggestions_details_section",
-                "fieldtype": "Section Break",
-                "label": "",
-                "insert_after": "skill_proficiency"
-            },
             {
                 "fieldname": "work_details",
                 "fieldtype": "Section Break",
@@ -1698,7 +1691,7 @@ def get_job_requisition_custom_fields():
                 "fieldtype": "Check",
                 "label": "Driving License Needed for this Position",
                 "depends_on": "eval:doc.travel_required == 1",
-                "insert_after": "min_experience",
+                "insert_after": "is_work_shift_needed",
                 "permlevel": 1
             },
             {
@@ -1834,7 +1827,7 @@ def get_job_requisition_custom_fields():
                 "fieldname": "suggestions",
                 "fieldtype": "Small Text",
                 "label": "Suggestions/Feedback",
-                "insert_after": "suggestions_details_section",
+                "insert_after": "description",
                 "permlevel": 3
             },
         ]
@@ -3520,8 +3513,8 @@ def get_property_setters():
             "doctype_or_field": "DocField",
             "doc_type": "Job Requisition",
             "property": "field_order",
-            "value": "[\"naming_series\", \"designation\", \"department\", \"column_break_qkna\", \"no_of_positions\", \"expected_compensation\",\"reason_for_requesting\", \"column_break_4\", \"company\", \"status\", \"section_break_7\", \"requested_by\", \"requested_by_name\", \"column_break_10\", \"requested_by_dept\", \"requested_by_designation\", \"timelines_tab\", \"posting_date\", \"completed_on\", \"column_break_15\", \"expected_by\", \"time_to_fill\", \"job_description_tab\", \"description\", \"connections_tab\"]"
-        },
+            "value": "[\"naming_series\", \"request_for\", \"employee_left\", \"relieving_date\", \"suggested_designation\", \"designation\", \"department\", \"employment_type\", \"location\", \"column_break_qkna\", \"no_of_positions\", \"expected_compensation\", \"reason_for_requesting\", \"column_break_4\", \"company\", \"status\", \"interview\", \"interview_rounds\", \"work_details\", \"no_of_days_off\", \"work_details_column_break\", \"travel_required\", \"is_work_shift_needed\", \"driving_license_needed\", \"license_type\", \"education\", \"min_education_qual\", \"education_column_break\", \"min_experience\", \"reset_column\", \"language_proficiency\", \"skill_proficiency\", \"section_break_7\", \"requested_by\", \"requested_by_name\", \"column_break_10\", \"requested_by_dept\", \"requested_by_designation\", \"timelines_tab\", \"posting_date\", \"completed_on\", \"column_break_15\", \"expected_by\", \"time_to_fill\", \"job_description_tab\", \"job_description_template\", \"job_title\", \"description\", \"suggestions\", \"connections_tab\"]"
+        }
     ]
 def get_material_request_custom_fields():
     '''
