9fce9d95cc5f23963230dcd4cae45d54faf434d3
Neha Fathima <nehaputhur443@gmail.com>
2025-03-10 16:01:52 +0530

    fix Removed the button when status become completed in Training Event doctype

--- beams/beams/custom_scripts/employee/employee.js
+++ beams/beams/custom_scripts/employee/employee.js
@@ -43,54 +43,53 @@ frappe.ui.form.on('Employee', {
           frm.set_value('leave_approver', '');
       }
   },
-  refresh: function(frm) {
+  refresh: function(frm){
     if (!frm.is_new() && frappe.user.has_role('HR Manager')) { // Adds the custom button 'Training Request' in the 'Create' section
-      frm.add_custom_button('Training Request', function() {
-        // Call the server-side function to fetch the employee ID for the current user
-        frappe.call({
-          method: "beams.beams.custom_scripts.employee.employee.get_employee_name_for_user",
-          args: {
-            user_id: frappe.session.user
-          },
-          callback: function(response) {
-            if (response.message) {
-              // If employee ID is found, create a new 'Training Request' document
-              frappe.new_doc('Training Request', {
-                employee: frm.doc.name,
-                training_requested_by: response.message // Set training_requested_by to the fetched employee ID
-              });
-            } else {
+		// Check if employee name exists for the logged-in user and add 'Event' button if available
+				frappe.call({
+					method: "beams.beams.custom_scripts.employee.employee.get_employee_name_for_user",
+					args: {
+						user_id: frappe.session.user
+					},
+					callback: function(response) {
+						if (response.message) {
+							frm.add_custom_button(__('Event'), function() {
+								frappe.model.open_mapped_doc({
+									method: "beams.beams.custom_scripts.employee.employee.create_event",
+									frm: frm,
+									args: {
+										"employee_id": frm.doc.name,
+										"hod_user": frappe.session.user
+									}
+								});
+							}, __('Create'));
 
-              // Show a message if no employee record is found for the user
-              frappe.msgprint(__('No employee record found for the current user.'));
-            }
-          }
-        });
-      }, 'Create');
+				      frm.add_custom_button('Training Request', function() {
+				        // Call the server-side function to fetch the employee ID for the current user
+				        frappe.call({
+				          method: "beams.beams.custom_scripts.employee.employee.get_employee_name_for_user",
+				          args: {
+				            user_id: frappe.session.user
+				          },
+				          callback: function(response) {
+				            if (response.message) {
+				              // If employee ID is found, create a new 'Training Request' document
+				              frappe.new_doc('Training Request', {
+				                employee: frm.doc.name,
+				                training_requested_by: response.message // Set training_requested_by to the fetched employee ID
+				              });
+				            } else {
 
-
-      // Check if employee name exists for the logged-in user and add 'Event' button if available
-      frappe.call({
-        method: "beams.beams.custom_scripts.employee.employee.get_employee_name_for_user",
-        args: {
-          user_id: frappe.session.user
-        },
-        callback: function(response) {
-          if (response.message) {
-            frm.add_custom_button(__('Event'), function() {
-              frappe.model.open_mapped_doc({
-                method: "beams.beams.custom_scripts.employee.employee.create_event",
-                frm: frm,
-                args: {
-                  "employee_id": frm.doc.name,
-                  "hod_user": frappe.session.user
-                }
-              });
-            }, __('Create'));
-          }
-        }
-      });
-    }
+				              // Show a message if no employee record is found for the user
+				              frappe.msgprint(__('No employee record found for the current user.'));
+				            }
+				          }
+				        });
+				      }, 'Create');
+						}
+					}
+    	});
+		}
   },
   employment_type: function(frm) {
     frappe.call({
