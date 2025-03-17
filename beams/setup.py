482a12ca013de89077ab43b632a457078e1ad240
Neha Fathima <nehaputhur443@gmail.com>
2025-03-17 14:08:44 +0530

    chore: removed unwanted tab in the code

--- beams/setup.py
+++ beams/setup.py
@@ -58,7 +58,6 @@ def after_install():
     create_custom_fields(get_asset_movement_custom_fields(),ignore_validate=True)
 
 
-
     #Creating BEAMS specific Property Setters
     create_property_setters(get_property_setters())
 
@@ -126,7 +125,6 @@ def before_uninstall():
     delete_custom_fields(get_asset_movement_custom_fields())
 
 
-
 def delete_custom_fields(custom_fields: dict):
     '''
     Method to Delete custom fields
@@ -168,7 +166,6 @@ def get_shift_assignment_custom_fields():
 
         ]
     }
-
 
 def get_Payroll_Settings_custom_fields():
     '''
