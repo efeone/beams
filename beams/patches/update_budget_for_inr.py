af0ac412a0722ec1cf62113971c1e6cd65b12a3c
Muhammed Sinan K T <91651425+MhmdSinanKT@users.noreply.github.com>
2025-03-17 16:10:01 +0530

    fix: added script in patch to update accoutns custom table

--- beams/patches/update_budget_for_inr.py
+++ beams/patches/update_budget_for_inr.py
@@ -4,6 +4,13 @@ def execute():
     print("Patch to update INR data in Budget")
     for budget in frappe.db.get_all("Budget", pluck="name"):
         try:
-            frappe.get_doc("Budget", budget).save()
+            doc = frappe.get_doc("Budget", budget)
+            doc.budget_accounts_custom = []
+            for row in doc.accounts:
+                budget_row = row.as_dict()
+                budget_row.pop('name')
+                budget_row.pop('parentfield')
+                doc.append('budget_accounts_custom', budget_row)
+            doc.save(ignore_permissions=True)
         except Exception as e:
             frappe.log_error("Error while updating Budget for INR via patch", e, "Budget", budget)
