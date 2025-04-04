e9f3a1e3cbfecc28d6e4bda26c25ab778f40677b
Avishna Murali <avishnachinnu26@gmail.com>
2025-04-04 10:21:38 +0530

    feat:Modify the vehicle request process

--- beams/beams/doctype/transportation_request/transportation_request.py
+++ beams/beams/doctype/transportation_request/transportation_request.py
@@ -38,6 +38,11 @@ class TransportationRequest(Document):
                     "hired_vehicle": vehicle.get("hired_vehicle", ""),
                     "reference_doctype": "Transportation Request",
                     "reference_name": self.name,
+                    "from":vehicle.from_location,
+                    "to":vehicle.to_location,
+                    "no_of_travellers":vehicle.no_of_travellers,
+                    "allocated":1
+
                 })
             project_doc.save(ignore_permissions=True)
 
