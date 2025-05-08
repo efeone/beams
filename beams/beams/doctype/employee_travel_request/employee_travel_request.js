eb47273caa68686dfe8fbead857d9185f28588aa
Zakkiyath Nissa <zakkiyathlulu@gmail.com>
2025-05-08 14:48:48 +0530

    chore:fixed alignment of code

--- beams/beams/doctype/employee_travel_request/employee_travel_request.js
+++ beams/beams/doctype/employee_travel_request/employee_travel_request.js
@@ -258,7 +258,7 @@ function set_vehicle_filters(frm, cdt, cdn) {
       .filter(row => row.name !== current_row.name && row.vehicle)
       .map(row => row.vehicle);
   
-      // Set query filter on the vehicle field to exclude selected vehicles
+  // Set query filter on the vehicle field to exclude selected vehicles
   frm.fields_dict.travel_vehicle_allocation.grid.get_field("vehicle").get_query = function(doc, cdt, cdn) {
       return {
           filters: [
