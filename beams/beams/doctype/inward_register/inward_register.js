a922b2754b2e08bc31f3c4e123ecf28c3e66e545
anshidakk <118747176+anshidakk@users.noreply.github.com>
2025-05-15 12:56:52 +0530

    feat: TASK-2025-00994 enable creating courier log from inward register (#971)

--- beams/beams/doctype/inward_register/inward_register.js
+++ beams/beams/doctype/inward_register/inward_register.js
@@ -5,21 +5,35 @@ frappe.ui.form.on('Inward Register', {
         if (frm.doc.docstatus === 1) {
             frm.add_custom_button(__('Outward Register'), function () {
                 frappe.new_doc("Outward Register", {
-                  inward_register: frm.doc.name,
+                    inward_register: frm.doc.name,
                 });
             }, __("Create"));
 
-            frm.add_custom_button(__(' Visitor Pass'), function () {
-                frappe.new_doc("Visitor Pass", {
-                    inward_register: frm.doc.name,
-                    issued_date: frappe.datetime.now_date(),
-                    issued_time: frappe.datetime.now_time(),
-                    issued_to: frm.doc.visitor_name
-                });
-            }, __("Create"));
+            if (!frappe.user.has_role('Security') || frappe.user.has_role('Administrator')) {
+                frm.add_custom_button(__(' Visitor Pass'), function () {
+                    frappe.new_doc("Visitor Pass", {
+                        inward_register: frm.doc.name,
+                        issued_date: frappe.datetime.now_date(),
+                        issued_time: frappe.datetime.now_time(),
+                        issued_to: frm.doc.visitor_name
+                    });
+                }, __("Create"));
+                if (frm.doc.visitor_type === 'Courier') {
+                    frm.add_custom_button(__('Courier Log'), function () {
+                        frappe.new_doc("Courier Log", {
+                            inward_register: frm.doc.name,
+                            sender: frm.doc.visitor_name,
+                            recipient: frm.doc.received_by,
+                            courier_service: frm.doc.courier_service,
+                            date: frm.doc.visit_date,
+                            description: frm.doc.purpose_of_visit
+                        });
+                    }, __("Create"));
+                }
+            }
         }
     },
-    posting_date:function (frm){
+    posting_date: function (frm) {
         frm.call("validate_posting_date");
-      }
+    }
 });
