93e4c16c192c76a0527badb6b2aafa2277feebbf
anshidakk <118747176+anshidakk@users.noreply.github.com>
2025-05-15 11:30:01 +0530

    feat: TASK-2025-01003 show button to create event when guest appointment approved (#972)

--- beams/beams/doctype/guest_appointment/guest_appointment.js
+++ beams/beams/doctype/guest_appointment/guest_appointment.js
@@ -2,34 +2,34 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Guest Appointment', {
-    refresh: function(frm) {
-        // Add a button to the 'Create' group to create a new Event
-        frm.add_custom_button(__('Event'), function() {
-            // Create a new Event DocType (empty Event)
-            frappe.new_doc('Event', {
-                guest_appointment: frm.doc.name  // Optionally link the Event to this Guest Appointment
-            });
-        }, 'Create');  // Add the button under the 'Create' group
-
-        if (frm.doc.workflow_state === "Approved") {
-          frm.add_custom_button(__('Inward Register'), function() {
-            frappe.call({
-              method: 'beams.beams.doctype.guest_appointment.guest_appointment.create_inward_register',
-                args: {
-                guest_appointment: frm.doc.name
-            },
-            callback: function(r) {
-                if (r.message) {
-                    frappe.db.get_doc('Inward Register', r.message).then(doc => {
-                        frappe.new_doc('Inward Register', doc);
-                    });
-                }
-            }
+  refresh: function (frm) {
+    // Add a button to the 'Create' group to create a new Event
+    if (frm.doc.workflow_state === "Approved") {
+      frm.add_custom_button(__('Event'), function () {
+        // Create a new Event DocType (empty Event)
+        frappe.new_doc('Event', {
+          guest_appointment: frm.doc.name  // Optionally link the Event to this Guest Appointment
         });
-        }, __("Create"));
-        }
-      },
-      posting_date:function (frm){
-        frm.call("validate_posting_date");
-      }      
+      }, 'Create');  // Add the button under the 'Create' group
+
+      frm.add_custom_button(__('Inward Register'), function () {
+        frappe.call({
+          method: 'beams.beams.doctype.guest_appointment.guest_appointment.create_inward_register',
+          args: {
+            guest_appointment: frm.doc.name
+          },
+          callback: function (r) {
+            if (r.message) {
+              frappe.db.get_doc('Inward Register', r.message).then(doc => {
+                frappe.new_doc('Inward Register', doc);
+              });
+            }
+          }
+        });
+      }, __("Create"));
+    }
+  },
+  posting_date: function (frm) {
+    frm.call("validate_posting_date");
+  }
 });
