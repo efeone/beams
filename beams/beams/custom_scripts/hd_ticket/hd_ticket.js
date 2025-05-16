55b9954a7fcd39c52e1c6be30e127adf239df72d
Arathi <arathipk98@gmail.com>
2025-05-16 12:47:56 +0530

    Add Child Table Spare Part Item in HD Ticket Doctype

--- beams/beams/custom_scripts/hd_ticket/hd_ticket.js
+++ beams/beams/custom_scripts/hd_ticket/hd_ticket.js
@@ -1,4 +1,8 @@
 frappe.ui.form.on('HD Ticket', {
+  /**
+     * Adds the 'Material Request' button under the 'Create' group
+     * if 'spare_part_needed' is checked.
+     */
     refresh: function(frm) {
         if (frm.doc.spare_part_needed) {
             frm.page.set_inner_btn_group_as_primary(__('Create'));
@@ -15,10 +19,13 @@ frappe.ui.form.on('HD Ticket', {
     }
 });
 
+// Function to add a custom button to create Material Request
 function add_material_request_button(frm) {
     frm.add_custom_button(__('Material Request'), function() {
+      // Create a new Material Request document
         let mr = frappe.model.get_new_doc('Material Request');
-
+           
+        // Populate the Material Request with items from 'spare_part_item_table'
         (frm.doc.spare_part_item_table || []).forEach(row => {
             let child = frappe.model.add_child(mr, 'Material Request Item', 'items');
             child.item_code = row.item;
