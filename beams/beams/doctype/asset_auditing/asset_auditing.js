d3658761b41df766993332a8b70f99f1998c8eaa
anshidakk <118747176+anshidakk@users.noreply.github.com>
2025-05-23 14:58:51 +0530

    feat: ability to scan qr code for asset in asset auditing (#1002)

--- beams/beams/doctype/asset_auditing/asset_auditing.js
+++ beams/beams/doctype/asset_auditing/asset_auditing.js
@@ -13,5 +13,20 @@ frappe.ui.form.on('Asset Auditing', {
     },
     posting_date:function (frm){
       frm.call("validate_posting_date");
+    },
+    // Fetch asset by scanned QR code and set 'asset' field
+    scan_qr_code: function(frm) {
+        if (frm.doc.scan_qr_code) {
+            frappe.db.get_value('Asset', frm.doc.scan_qr_code, 'name')
+                .then(r => {
+                    if (r && r.message) {
+                        frm.set_value('asset', r.message.name);
+                    } else {
+                        frappe.msgprint(__('Asset not found for scanned QR code'));
+                        frm.set_value('asset', null);
+                    }
+                    frm.set_value('scan_qr_code', null);
+                });
+        }
     }
 });
