dacf82061563eda1fe0b59d1052aa9727d576f89
Avishna Murali <105269795+avishna-avi@users.noreply.github.com>
2025-05-28 14:18:17 +0530

    feat: validation for attachments in travel request and new fields in jv and vehicle (#1008)

--- beams/beams/custom_scripts/vehicle/vehicle.js
+++ beams/beams/custom_scripts/vehicle/vehicle.js
@@ -1,11 +1,11 @@
 frappe.ui.form.on('Vehicle', {
     refresh: function(frm) {
-        if (!frm.is_new()) {
-            frm.add_custom_button(__('New Vehicle Safety Inspection'), function () {
-                frappe.new_doc('Vehicle Safety Inspection', {
+        frm.set_query('vehicle_safety_inspection', () => {
+            return {
+                filters: {
                     vehicle: frm.doc.name
-                });
-            }, __('Create'));
-        }
+                }
+            };
+        });
     }
 });
