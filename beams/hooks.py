ef81ef7c6d9f728eb6c2a927255ca4707fe2efad
Avishna Murali <avishnachinnu26@gmail.com>
2025-05-31 13:04:56 +0530

    feat:ensure QR code is generated for all assets created from Purchase Receipt

--- beams/hooks.py
+++ beams/hooks.py
@@ -358,8 +358,10 @@ doc_events = {
         "before_save": "beams.beams.custom_scripts.asset_movement.asset_movement.before_save",
     },
     "Asset":{
-        "after_insert":"beams.beams.custom_scripts.asset.asset.generate_asset_qr",
-        "on_submit":"beams.beams.custom_scripts.asset.asset.generate_asset_details_qr",
+        "on_submit": [
+            "beams.beams.custom_scripts.asset.asset.generate_asset_qr",
+            "beams.beams.custom_scripts.asset.asset.generate_asset_details_qr"
+        ],
         "on_update_after_submit":"beams.beams.custom_scripts.asset.asset.create_asset_location_log"
     },
     "Budget":{
