ea2e436c611efc9d43c4a6fc24cc06cdd4f01281
Anshida <anshida41@gmail.com>
2025-06-05 14:48:38 +0530

    feat:hide some fields in the asset movement child table depends on the purpose

--- beams/beams/custom_scripts/asset_movement/asset_movement.js
+++ beams/beams/custom_scripts/asset_movement/asset_movement.js
@@ -9,6 +9,7 @@ frappe.ui.form.on('Asset Movement', {
         });
     },
     refresh: function(frm) {
+        frm.trigger('toggle_asset_movement_item_fields');
         // Filter for 'Shelf','Row' & 'Bin' field in assets child table
         frm.fields_dict.assets.grid.get_field('shelf').get_query = function(doc, cdt, cdn) {
             let row = locals[cdt][cdn];
@@ -34,6 +35,19 @@ frappe.ui.form.on('Asset Movement', {
                 }
             };
         };
-    }
+    },
+    purpose: function(frm) {
+        // Call toggle_fields when purpose changes
+        frm.trigger('toggle_asset_movement_item_fields');
+    },
 
+    toggle_asset_movement_item_fields: function(frm) {
+        // Hide Room, Shelf, Row, and Bin fields in Asset Movement Item when purpose is 'Issue'
+        const hide_fields = (frm.doc.purpose === 'Issue');
+
+        frm.fields_dict.assets.grid.update_docfield_property('room', 'hidden', hide_fields);
+        frm.fields_dict.assets.grid.update_docfield_property('shelf', 'hidden', hide_fields);
+        frm.fields_dict.assets.grid.update_docfield_property('row', 'hidden', hide_fields);
+        frm.fields_dict.assets.grid.update_docfield_property('bin', 'hidden', hide_fields);
+    }
 });
