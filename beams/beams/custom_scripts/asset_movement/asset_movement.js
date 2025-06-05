0ea28c8752a784e61abbcfafbdc391edd8c47c7b
Anshida <anshida41@gmail.com>
2025-06-05 13:08:52 +0530

    feat:add filter for shelf,row and bin in assets and asset movement

--- beams/beams/custom_scripts/asset_movement/asset_movement.js
+++ beams/beams/custom_scripts/asset_movement/asset_movement.js
@@ -7,5 +7,33 @@ frappe.ui.form.on('Asset Movement', {
         frm.set_query('reference_name', function() {
             return { filters: {} };
         });
+    },
+    refresh: function(frm) {
+        // Filter for 'Shelf','Row' & 'Bin' field in assets child table
+        frm.fields_dict.assets.grid.get_field('shelf').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return {
+                filters: {
+                    'room': row.room
+                }
+            };
+        };
+        frm.fields_dict.assets.grid.get_field('row').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return {
+                filters: {
+                    'shelf': row.shelf
+                }
+            };
+        };
+        frm.fields_dict.assets.grid.get_field('bin').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            return {
+                filters: {
+                    'row': row.row
+                }
+            };
+        };
     }
+
 });
