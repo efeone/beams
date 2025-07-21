7d35a1d7bc0d03a4161a3ce55af769c4b0bf61a8
Neha Fathima <nehaputhur443@gmail.com>
2025-07-21 12:39:49 +0530

    fix: need to fix fetch the shift type from shift assignement in employee checkin permisssion

--- beams/beams/doctype/employee_checkin_permission/employee_checkin_permission.js
+++ beams/beams/doctype/employee_checkin_permission/employee_checkin_permission.js
@@ -1,8 +1,46 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Checkin Permission", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee Checkin Permission", {
+	onload(frm) {
+        if (!frm.is_new()|| frm.doc.employee) return;
+        if(frappe.session.user !== "Administrator"){
+            frappe.call({
+                method:'beams.beams.doctype.employee_checkin_permission.employee_checkin_permission.get_employee_for_current_user',
+                callback: function(r){
+                    if(r.message){
+                        frm.set_value('employee',r.message);
+                    }else {
+                        frappe.msgprint(__('No Employee record linked to this user.'));
+                    }
+                }
+            });
+        }
+	},
+    employee: function(frm){
+        set_shift_based_on_assignment(frm);
+    },
+    date: function(frm){
+        set_shift_based_on_assignment(frm);
+    }
+});
 
-// 	},
-// });
+function set_shift_based_on_assignment(frm) {
+    /**
+    * Sets the shift field on the form based on the employee and date by fetching the assigned shift using a server-side method.
+    */
+    if (frm.doc.employee && frm.doc.date) {
+        frappe.call({
+            method: 'beams.beams.doctype.employee_checkin_permission.employee_checkin_permission.get_shift_for_employee_on_date',
+            args: {
+                employee: frm.doc.employee,
+                date: frm.doc.date
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frm.set_value('shift', r.message);
+                }           
+            }
+        });
+    }
+}
--- beams/beams/doctype/employee_checkin_permission/employee_checkin_permission.py
+++ beams/beams/doctype/employee_checkin_permission/employee_checkin_permission.py
@@ -1,9 +1,37 @@
 # Copyright (c) 2025, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EmployeeCheckinPermission(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_employee_for_current_user():
+    """Fetches the Employee name linked to the currently logged-in user."""
+    employee = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "name")
+    return employee
+
+
+@frappe.whitelist()
+def get_shift_for_employee_on_date(employee, date):
+    """
+    Returns the assigned shift for the employee on the given date
+    if there is a Shift Assignment. Returns None if no assignment is found.
+    """
+    shift = frappe.db.get_value(
+        "Shift Assignment",
+        {
+            "employee": employee,
+            "status": "Active",
+            "start_date": ["<=", date],
+            "end_date": [">=", date]
+        },
+        "shift_type"
+    )
+    if not shift:
+         shift = frappe.db.get_value("Employee",employee,"default_shift")
+    return shift
