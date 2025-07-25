0325e635ff4aac880773199512c16856800338e6
anjusha <anjushasuresh85@gmail.com>
2025-07-25 14:44:46 +0530

    chore: code formatting

--- beams/beams/custom_scripts/job_applicant/job_applicant.js
+++ beams/beams/custom_scripts/job_applicant/job_applicant.js
@@ -9,23 +9,23 @@ frappe.ui.form.on('Job Applicant', {
             'Local Enquiry Rejected', 'Local Enquiry Completed', 'Accepted', 'Training Completed', 'Compensation Proposal Accepted', 'Compensation Proposal Created'
         ];
 
-        if (statuses.includes(frm.doc.status)) {
-            frm.remove_custom_button(__('Local Enquiry Report'), __('Create'));
-        }
+		if (statuses.includes(frm.doc.status)) {
+			frm.remove_custom_button(__('Local Enquiry Report'), __('Create'));
+		}
 
-        if (frm.doc.status === 'Accepted' || frm.doc.status === 'Training Completed') {
-            // Remove "View" button and its inner options
-            frm.remove_custom_button('View');
-            frm.page.remove_inner_button('Job Offer', 'View');
+		if (frm.doc.status === 'Accepted' || frm.doc.status === 'Training Completed') {
+			// Remove "View" button and its inner options
+			frm.remove_custom_button('View');
+			frm.page.remove_inner_button('Job Offer', 'View');
 
-            // Remove "Send Magic Link" button
-            frm.remove_custom_button('Send Magic Link');
+			// Remove "Send Magic Link" button
+			frm.remove_custom_button('Send Magic Link');
 
-            // Remove "Rejected" and "Hold" options under "Set Status" button
-            frm.page.remove_inner_button('Rejected', 'Set Status');
-            frm.page.remove_inner_button('Hold', 'Set Status');
-            frm.remove_custom_button(__('Job Offer'), __('Create'));
-        }
+			// Remove "Rejected" and "Hold" options under "Set Status" button
+			frm.page.remove_inner_button('Rejected', 'Set Status');
+			frm.page.remove_inner_button('Hold', 'Set Status');
+			frm.remove_custom_button(__('Job Offer'), __('Create'));
+		}
 
         const magic_link_statuses = [
             'Interview Completed', 'Local Enquiry Approved', 'Selected',
@@ -33,39 +33,39 @@ frappe.ui.form.on('Job Applicant', {
             'Interview Scheduled', 'Interview Ongoing'
         ];
 
-        if (magic_link_statuses.includes(frm.doc.status)) {
-            frm.remove_custom_button('Send Magic Link');
-        }
+		if (magic_link_statuses.includes(frm.doc.status)) {
+			frm.remove_custom_button('Send Magic Link');
+		}
 
         if (frm.doc.status === 'Compensation Proposal Created' || frm.doc.status === 'Compensation Proposal Accepted') {
             frm.page.remove_inner_button('Rejected', 'Set Status');
             frm.page.remove_inner_button('Hold', 'Set Status');
         }
 
-        if (frm.doc.status === 'Interview Scheduled' || frm.doc.status === 'Interview Ongoing') {
-            frm.page.remove_inner_button('Local Enquiry Report', 'Create');
-        }
-    },
-    status: function (frm) {
-        frm.trigger('refresh');
-    }
+		if (frm.doc.status === 'Interview Scheduled' || frm.doc.status === 'Interview Ongoing') {
+			frm.page.remove_inner_button('Local Enquiry Report', 'Create');
+		}
+	},
+	status: function (frm) {
+		frm.trigger('refresh');
+	}
 });
 
 function handle_custom_buttons(frm) {
-    if (!frm.is_new()) {
-        if (frappe.user_roles.includes('HR Manager')) {
-            // Handle "Appointment Letter" button for "Training Completed" status
-            if (frm.doc.status === 'Training Completed') {
-                frappe.db.get_value('Appointment Letter', { 'job_applicant': frm.doc.name }, 'name', function (result) {
-                    if (!result || !result.name) {
-                        frm.add_custom_button(__('Appointment Letter'), function () {
-                            frappe.new_doc('Appointment Letter', {
-                                job_applicant: frm.doc.name
-                            });
-                        }, __('Create'));
-                    }
-                });
-            }
+	if (!frm.is_new()) {
+		if (frappe.user_roles.includes('HR Manager')) {
+			// Handle "Appointment Letter" button for "Training Completed" status
+			if (frm.doc.status === 'Training Completed') {
+				frappe.db.get_value('Appointment Letter', { 'job_applicant': frm.doc.name }, 'name', function (result) {
+					if (!result || !result.name) {
+						frm.add_custom_button(__('Appointment Letter'), function () {
+							frappe.new_doc('Appointment Letter', {
+								job_applicant: frm.doc.name
+							});
+						}, __('Create'));
+					}
+				});
+			}
 
             // Handle "Compensation Proposal" button for "Selected" status
             if (frm.doc.status === 'Selected') {
@@ -82,8 +82,8 @@ function handle_custom_buttons(frm) {
                 });
             }
 
-            // Show a "Local Enquiry Report (LER)" button in the Job Applicant form only when the status is "Interview Completed"
-            if (frm.doc.status == 'Shortlisted from Interview') {
+			// Show a "Local Enquiry Report (LER)" button in the Job Applicant form only when the status is "Shortlisted from Interview"
+			if (frm.doc.status == 'Shortlisted from Interview') {
 				frappe.call({
 					method: 'beams.beams.custom_scripts.job_applicant.job_applicant.get_existing_local_enquiry_report',
 					args: {
@@ -118,100 +118,100 @@ function handle_custom_buttons(frm) {
 				});
 			}
 
-            if (frm.doc.status == 'Shortlisted' || frm.doc.status == 'Pending Document Upload') {
-                frm.add_custom_button(__('Send Magic Link'), function () {
-                    frappe.confirm('Are you sure you want to send the magic link to the candidate?', function () {
-                        frappe.call({
-                            method: 'beams.beams.custom_scripts.job_applicant.job_applicant.send_magic_link',
-                            args: {
-                                applicant_id: frm.doc.name
-                            },
-                            callback: function (r) {
-                                if (r.message) {
-                                    navigator.clipboard.writeText(r.message).then(function () {
-                                        frappe.show_alert(__('Magic Link copied to clipboard!'));
-                                    }).catch(function (err) {
-                                        frappe.show_alert(__('Failed to copy Magic Link to clipboard'));
-                                    });
-                                }
-                            }
-                        });
-                    });
-                });
-            }
+			if (frm.doc.status == 'Shortlisted' || frm.doc.status == 'Pending Document Upload') {
+				frm.add_custom_button(__('Send Magic Link'), function () {
+					frappe.confirm('Are you sure you want to send the magic link to the candidate?', function () {
+						frappe.call({
+							method: 'beams.beams.custom_scripts.job_applicant.job_applicant.send_magic_link',
+							args: {
+								applicant_id: frm.doc.name
+							},
+							callback: function (r) {
+								if (r.message) {
+									navigator.clipboard.writeText(r.message).then(function () {
+										frappe.show_alert(__('Magic Link copied to clipboard!'));
+									}).catch(function (err) {
+										frappe.show_alert(__('Failed to copy Magic Link to clipboard'));
+									});
+								}
+							}
+						});
+					});
+				});
+			}
 
-            if (frm.doc.status === 'Accepted') {
-                frm.add_custom_button(__('Training Completed'), function () {
-                    frm.set_value('status', 'Training Completed');
-                    frm.save();
-                }, __('Set Status'));
-            }
+			if (frm.doc.status === 'Accepted') {
+				frm.add_custom_button(__('Training Completed'), function () {
+					frm.set_value('status', 'Training Completed');
+					frm.save();
+				}, __('Set Status'));
+			}
 
-            if (frm.doc.status !== 'Rejected' && frm.doc.status !== 'Selected') {
-                frm.add_custom_button(__('Rejected'), function () {
-                    frm.set_value('status', 'Rejected');
-                    frm.save();
-                }, __('Set Status'));
-            }
+			if (frm.doc.status !== 'Rejected' && frm.doc.status !== 'Selected') {
+				frm.add_custom_button(__('Rejected'), function () {
+					frm.set_value('status', 'Rejected');
+					frm.save();
+				}, __('Set Status'));
+			}
 
-            // Show "Hold" button only if status is neither "Rejected" nor "Hold"
-            if (frm.doc.status !== 'Hold' && frm.doc.status !== 'Rejected' && frm.doc.status !== 'Selected') {
-                frm.add_custom_button(__('Hold'), function () {
-                    frm.set_value('status', 'Hold');
-                    frm.save();
-                }, __('Set Status'));
-            }
+			// Show "Hold" button only if status is neither "Rejected" nor "Hold"
+			if (frm.doc.status !== 'Hold' && frm.doc.status !== 'Rejected' && frm.doc.status !== 'Selected') {
+				frm.add_custom_button(__('Hold'), function () {
+					frm.set_value('status', 'Hold');
+					frm.save();
+				}, __('Set Status'));
+			}
 
-            if (['Hold', 'Local Enquiry Approved'].includes(frm.doc.status)) {
-                frm.add_custom_button(__('Selected'), function () {
-                    frm.set_value('status', 'Selected');
-                    frm.save();
-                }, __('Set Status'));
-            }
+			if (['Hold', 'Local Enquiry Approved'].includes(frm.doc.status)) {
+				frm.add_custom_button(__('Selected'), function () {
+					frm.set_value('status', 'Selected');
+					frm.save();
+				}, __('Set Status'));
+			}
 
-            if (frm.doc.status === 'Open') {
-                frm.add_custom_button(__('Shortlisted'), function () {
-                    frm.set_value('status', 'Shortlisted');
-                    frm.save();
-                }, __('Set Status'));
-            }
+			if (frm.doc.status === 'Open') {
+				frm.add_custom_button(__('Shortlisted'), function () {
+					frm.set_value('status', 'Shortlisted');
+					frm.save();
+				}, __('Set Status'));
+			}
 
-            if (frm.doc.status == 'Interview Completed') {
-                frm.add_custom_button(__('Shortlisted from Interview'), function() {
-                    frm.set_value('status', 'Shortlisted from Interview');
-                    frm.save();
-                }, __('Set Status'));
-            }
-        }
-    }
+			if (frm.doc.status == 'Interview Completed') {
+				frm.add_custom_button(__('Shortlisted from Interview'), function() {
+					frm.set_value('status', 'Shortlisted from Interview');
+					frm.save();
+				}, __('Set Status'));
+			}
+		}
+	}
 }
 
 frappe.ui.form.on('Applicant Interview Round', {
-    create_interview: function (frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        // Check if the form is dirty
-        if (frm.is_dirty()) {
-            frappe.msgprint(__('Please save your changes before creating or viewing an interview.'));
-            return;
-        }
-        // View created Interviews. If no Interview exists, open a new unsaved Interview form with pre-filled values
-        if (row.interview_reference) {
-            frappe.set_route('Form', 'Interview', row.interview_reference);
-        }
-        else {
-            if (!["Document Uploaded", "Interview Scheduled", "Interview Ongoing"].includes(frm.doc.status)){
-                frappe.msgprint(__('Please upload the required documents before creating or viewing an interview.'));
-                return;
-            }
-            frappe.model.with_doctype('Interview', function () {
-                let new_interview = frappe.model.get_new_doc('Interview');
-                new_interview.job_applicant = frm.doc.name;
-                new_interview.job_title = frm.doc.job_opening;
-                new_interview.interview_round = row.interview_round;
-                new_interview.scheduled_on = frappe.datetime.now_date();
-                new_interview.status = 'Pending';
-                frappe.set_route('Form', 'Interview', new_interview.name);
-            });
-        }
-    }
+	create_interview: function (frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		// Check if the form is dirty
+		if (frm.is_dirty()) {
+			frappe.msgprint(__('Please save your changes before creating or viewing an interview.'));
+			return;
+		}
+		// View created Interviews. If no Interview exists, open a new unsaved Interview form with pre-filled values
+		if (row.interview_reference) {
+			frappe.set_route('Form', 'Interview', row.interview_reference);
+		}
+		else {
+			if (!["Document Uploaded", "Interview Scheduled", "Interview Ongoing"].includes(frm.doc.status)){
+				frappe.msgprint(__('Please upload the required documents before creating or viewing an interview.'));
+				return;
+			}
+			frappe.model.with_doctype('Interview', function () {
+				let new_interview = frappe.model.get_new_doc('Interview');
+				new_interview.job_applicant = frm.doc.name;
+				new_interview.job_title = frm.doc.job_opening;
+				new_interview.interview_round = row.interview_round;
+				new_interview.scheduled_on = frappe.datetime.now_date();
+				new_interview.status = 'Pending';
+				frappe.set_route('Form', 'Interview', new_interview.name);
+			});
+		}
+	}
 });
