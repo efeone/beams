667f69ec7610c50346d77c5898ceb82e1b6dc23a
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-08-06 17:04:47 +0530

    refactor: key log

--- beams/beams/doctype/key_log/key_log.js
+++ beams/beams/doctype/key_log/key_log.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Vehicle Key Log", {
+// frappe.ui.form.on("Key Log", {
 // 	refresh(frm) {
 
 // 	},
--- beams/beams/doctype/key_log/key_log.py
+++ beams/beams/doctype/key_log/key_log.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class VehicleKeyLog(Document):
+class KeyLog(Document):
 	pass
