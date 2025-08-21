3393bd0fcb59400d612f863842ec117578b30438
anjusha <anjushasuresh85@gmail.com>
2025-08-21 17:38:10 +0530

    chore: code cleanup

--- beams/beams/doctype/visitor_pass/visitor_pass.py
+++ beams/beams/doctype/visitor_pass/visitor_pass.py
@@ -8,65 +8,65 @@ from frappe.model.document import Document
 
 class VisitorPass(Document):
 
-    def validate(self):
-        self.validate_issued_date_and_expire_on()
-        self.validate_issued_date_and_returned_date()
+	def validate(self):
+		self.validate_issued_date_and_expire_on()
+		self.validate_issued_date_and_returned_date()
 
-    def on_update_after_submit(self):
-        self.validate_returned_date_and_returned_time()
+	def on_update_after_submit(self):
+		self.validate_returned_date_and_returned_time()
 
-    def validate_returned_date_and_returned_time(self):
-        '''
-        Ensure Returned Date and Time selection based on workflow state
-        '''
-        if self.workflow_state == 'Returned':
-            if not self.returned_date:
-                frappe.throw(_('Please select a Returned Date and Time.'))
+	def validate_returned_date_and_returned_time(self):
+		'''
+		Ensure Returned Date and Time selection based on workflow state
+		'''
+		if self.workflow_state == 'Returned':
+			if not self.returned_date:
+				frappe.throw(_('Please select a Returned Date and Time.'))
 
-    @frappe.whitelist()
-    def validate_issued_date_and_expire_on(self):
-        '''
-        Validates that issued_date and expire_on are properly set and checks
-        if issued_date is not later than expire_on.
-        '''
-        if not self.issued_date or not self.expire_on:
-            return
+	@frappe.whitelist()
+	def validate_issued_date_and_expire_on(self):
+		'''
+		Validates that issued_date and expire_on are properly set and checks
+		if issued_date is not later than expire_on.
+		'''
+		if not self.issued_date or not self.expire_on:
+			return
 
-        issued_date = getdate(self.issued_date)
-        expire_on = getdate(self.expire_on)
+		issued_date = getdate(self.issued_date)
+		expire_on = getdate(self.expire_on)
 
-        if issued_date > expire_on:
-            frappe.throw(
-                msg=_("Issued Date cannot be after Expire On."),
-                title=_("Validation Error")
-            )
+		if issued_date > expire_on:
+			frappe.throw(
+				msg=_("Issued Date cannot be after Expire On."),
+				title=_("Validation Error")
+			)
 
-    @frappe.whitelist()
-    def validate_issued_date_and_returned_date(self):
-        '''
-        Validates that issued_date and returned_date are properly set and checks
-        if issued_date is not later than returned_date.
-        Also checks that issued_time is not later than returned_time if both dates are the same.
-        '''
-        if not self.issued_date or not self.returned_date:
-            return
+	@frappe.whitelist()
+	def validate_issued_date_and_returned_date(self):
+		'''
+		Validates that issued_date and returned_date are properly set and checks
+		if issued_date is not later than returned_date.
+		Also checks that issued_time is not later than returned_time if both dates are the same.
+		'''
+		if not self.issued_date or not self.returned_date:
+			return
 
-        issued_date = getdate(self.issued_date)
-        returned_date = getdate(self.returned_date)
+		issued_date = getdate(self.issued_date)
+		returned_date = getdate(self.returned_date)
 
-        if issued_date > returned_date:
-            frappe.throw(
-                msg=_("Returned Date cannot be before Issued Date."),
-                title=_("Validation Error")
-            )
+		if issued_date > returned_date:
+			frappe.throw(
+				msg=_("Returned Date cannot be before Issued Date."),
+				title=_("Validation Error")
+			)
 
-        if issued_date == returned_date:
-            if self.issued_time and self.returned_date:
-                issued_time = get_time(self.issued_time)
-                returned_time = get_time(self.returned_date)
+		if issued_date == returned_date:
+			if self.issued_time and self.returned_date:
+				issued_time = get_time(self.issued_time)
+				returned_time = get_time(self.returned_date)
 
-                if issued_time >= returned_time:
-                    frappe.throw(
-                        msg=_("Issued Time must be greater than Returned Time "),
-                        title=_("Validation Error")
-                    )
+				if issued_time >= returned_time:
+					frappe.throw(
+						msg=_("Issued Time must be greater than Returned Time "),
+						title=_("Validation Error")
+					)
