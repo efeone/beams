4a8302e189180348fa071402d99eb81dd1fecef1
anjusha <anjushasuresh85@gmail.com>
2025-08-21 15:50:08 +0530

    chore: code cleanup

--- beams/beams/doctype/inward_register/inward_register.py
+++ beams/beams/doctype/inward_register/inward_register.py
@@ -10,28 +10,27 @@ from frappe.utils.user import get_users_with_role
 
 class InwardRegister(Document):
 
-    def before_save(self):
-        self.validate_posting_date()
+	def before_save(self):
+		self.validate_posting_date()
 
-    def validate(self):
-        if self.visitor_type == "Ex Employee":
-            if not self.visitor_name or not self.visit_date:
-                frappe.throw("Visitor Name and Visit Date are required for Ex Employees.")
+	def validate(self):
+		if self.visitor_type == "Ex Employee":
+			if not self.visitor_name or not self.visit_date:
+				frappe.throw("Visitor Name and Visit Date are required for Ex Employees.")
 
-            # Check if a Visit Request exists
-            visit_request = frappe.db.exists(
-                "Visit Request",
-                {
-                    "visitor_name": self.visitor_name,
-                    "visit_date": self.visit_date
-                }
-            )
+			# Check if a Visit Request exists
+			visit_request = frappe.db.exists(
+				"Visit Request",
+				{
+					"visitor_name": self.visitor_name,
+					"visit_date": self.visit_date
+				}
+			)
+			if not visit_request:
+				frappe.throw(f"No Visit Request found for {self.visitor_name} on {self.visit_date}.")
 
-            if not visit_request:
-                frappe.throw(f"No Visit Request found for {self.visitor_name} on {self.visit_date}.")
-
-    @frappe.whitelist()
-    def validate_posting_date(self):
-        if self.posting_date:
-            if frappe.utils.get_datetime(self.posting_date) > frappe.utils.get_datetime():
-                frappe.throw(_("Posting Date cannot be set after Now date."))
+	@frappe.whitelist()
+	def validate_posting_date(self):
+		if self.posting_date:
+			if frappe.utils.get_datetime(self.posting_date) > frappe.utils.get_datetime():
+				frappe.throw(_("Posting Date cannot be set after Now date."))
