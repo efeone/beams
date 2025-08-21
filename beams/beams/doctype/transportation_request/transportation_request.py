b53f522965c654b667bb4f03e92306d2dc358818
NaseeraVP <naseeravp38@gmail.com>
2025-08-21 16:29:33 +0530

    refactor:Changed Controller Hook based on Workflow Updation

--- beams/beams/doctype/transportation_request/transportation_request.py
+++ beams/beams/doctype/transportation_request/transportation_request.py
@@ -3,22 +3,24 @@
 import frappe
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
-from frappe.utils import today
+from frappe.utils import today, getdate
 from frappe import _
-
 
 class TransportationRequest(Document):
     def before_save(self):
         self.validate_posting_date()
 
-    def on_cancel(self):
+    def validate(self):
         if self.workflow_state == "Rejected" and not self.reason_for_rejection:
             frappe.throw("Please provide a Reason for Rejection before rejecting this request.")
 
     def before_update_after_submit(self):
         self.update_no_of_own_vehicles()
 
-    def on_update_after_submit(self):
+    def on_submit(self):
+        # Always update own vehicles count on workflow update
+        self.update_no_of_own_vehicles()
+
         if self.workflow_state == "Approved" and self.reason_for_rejection:
             frappe.throw("You cannot approve this request if 'Reason for Rejection' is filled.", title="Approval Error")
 
@@ -38,7 +40,6 @@ class TransportationRequest(Document):
             for existing_vehicle in existing_vehicles:
                 if existing_vehicle.reference_name != self.name:
                     updated_vehicle_details.append(existing_vehicle)
-
                 elif existing_vehicle.vehicle in vehicles_to_update:
                     vehicle = vehicles_to_update[existing_vehicle.vehicle]
                     updated_vehicle_details.append({
@@ -77,16 +78,15 @@ class TransportationRequest(Document):
         Calculate the total number of rows in the "Vehicles" child table
         and update the "No. of Own Vehicles" field.
         '''
-
         total_vehicles = len(self.vehicles or [])
         self.no_of_own_vehicles = total_vehicles
 
     @frappe.whitelist()
     def validate_posting_date(self):
         if self.posting_date:
-            if self.posting_date > today():
+            posting_date = getdate(self.posting_date)  # Convert to date object
+            if posting_date > getdate(today()):
                 frappe.throw(_("Posting Date cannot be set after today's date."))
-
 
 @frappe.whitelist()
 def map_transportation_to_vehicle(source_name, target_doc=None):
