3919928a72fecca18226d7e2b6021443df07844d
Avishna Murali <avishnachinnu26@gmail.com>
2025-08-23 11:26:36 +0530

    fix:correct date validation in Equipment Request

--- beams/beams/doctype/equipment_request/equipment_request.py
+++ beams/beams/doctype/equipment_request/equipment_request.py
@@ -86,7 +86,7 @@ class EquipmentRequest(Document):
     @frappe.whitelist()
     def validate_posting_date(self):
         if self.posting_date:
-            if self.posting_date > today():
+            if getdate(self.posting_date) > getdate(today()):
                 frappe.throw(_("Posting Date cannot be set after today's date."))
 
 
