318a561ec5f8060566b4c724613682ebacfd4e02
Avishna Murali <avishnachinnu26@gmail.com>
2025-09-01 10:06:52 +0530

    fix: allow Asset Movement without Employee user permission check

--- beams/setup.py
+++ beams/setup.py
@@ -5086,7 +5086,8 @@ def get_asset_movement_custom_fields():
 				"label": "New Custodian",
 				"options": "Employee",
 				"insert_after": "assets",
-				"read_only": 1
+				"read_only": 1,
+				"ignore_user_permissions":1
 			},
 			{
 				"fieldname": "user_id",
