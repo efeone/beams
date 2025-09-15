e85f0c3fdadc44246fbf795a8c664b2df39c69a9
NaseeraVP <naseeravp38@gmail.com>
2025-09-15 13:28:33 +0530

    feat:Customise Front Desk User role

--- beams/setup.py
+++ beams/setup.py
@@ -240,11 +240,11 @@ def get_hd_ticket_custom_fields():
 				"insert_after": "ticket_section_break"
 			},
 			{
-				"fieldname": "spare_part_item_table",
+				"fieldname": "material_request_items",
 				"fieldtype": "Table",
-				"label": "Spare Part Items",
+				"label": "Material Request Items",
 				"insert_after": "material_request_needed",
-				"options": "Spare Part Item",
+				"options": "Material Request Items",
 				"depends_on": "eval:doc.material_request_needed == 1"
 			}
 		]
@@ -5103,7 +5103,7 @@ def get_beams_roles():
 	'''
 		Method to get BEAMS specific roles
 	'''
-	return ['Production Manager', 'CEO', 'Company Secretary', 'HOD','Enquiry Officer','Enquiry Manager','Shift Publisher','Program Producer','Operations Head','Operations User','Admin','Driver','Budget User','Technical Store Head','Budget Verifier','Budget Verifier Finance','Budget Approver','Admin User','Bureau User','Coordinating Editor','News Coordinator','Security','Reporter','Salary Increment Approver']
+	return ['Production Manager', 'CEO', 'Company Secretary', 'HOD','Enquiry Officer','Enquiry Manager','Shift Publisher','Program Producer','Operations Head','Operations User','Admin','Driver','Budget User','Technical Store Head','Budget Verifier','Budget Verifier Finance','Budget Approver','Admin User','Bureau User','Coordinating Editor','News Coordinator','Security','Reporter','Salary Increment Approver','Front Desk User']
 
 def get_custom_translations():
 	'''
