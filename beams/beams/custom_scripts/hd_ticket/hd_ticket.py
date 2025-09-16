2888525b648cd8d3406a60e1b4698d29f4f45e24
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-09-16 18:59:21 +0530

    fix: proper routing and documentation

--- beams/beams/custom_scripts/hd_ticket/hd_ticket.py
+++ beams/beams/custom_scripts/hd_ticket/hd_ticket.py
@@ -4,106 +4,106 @@ from helpdesk.helpdesk.doctype.hd_ticket.hd_ticket import HDTicket
 
 class HDTicketOverride(HDTicket):
 
-    def on_update(self):
-        '''Extend on_update to auto-assign when ticket is Open.'''
+	def on_update(self):
+		'''Extend on_update to auto-assign when ticket is Open.'''
 
-        super().on_update()
-        if self.agent_group and self.status == 'Open':
-            self.handle_assignment_by_team()
+		super().on_update()
+		if self.agent_group and self.status == 'Open':
+			self.handle_assignment_by_team()
 
-    def validate(self):
-        '''Extend validate to set agent group automatically.'''
+	def validate(self):
+		'''Extend validate to set agent group automatically.'''
 
-        super().validate()
-        self.set_agent_group()
+		super().validate()
+		self.set_agent_group()
 
-    def handle_assignment_by_team(self):
-        '''Assign ticket to all active agents in the selected agent group.'''
+	def handle_assignment_by_team(self):
+		'''Assign ticket to all active agents in the selected agent group.'''
 
-        if not self.agent_group:
-            return
+		if not self.agent_group:
+			return
 
-        if not frappe.db.exists('HD Team', self.agent_group):
-            return
+		if not frappe.db.exists('HD Team', self.agent_group):
+			return
 
-        # Fetch all active users from the team
-        active_users = self.get_active_users_from_team(self.agent_group)
-        if not active_users:
-            return
+		# Fetch all active users from the team
+		active_users = self.get_active_users_from_team(self.agent_group)
+		if not active_users:
+			return
 
-        # Assign to all active agents
-        for user in active_users:
-            existing_todo = frappe.db.exists('ToDo', {
-                'reference_type': self.doctype,
-                'reference_name': self.name,
-                'owner': user,
-                'status': ['!=', 'Cancelled'],
-            })
+		# Assign to all active agents
+		for user in active_users:
+			existing_todo = frappe.db.exists('ToDo', {
+				'reference_type': self.doctype,
+				'reference_name': self.name,
+				'owner': user,
+				'status': ['!=', 'Cancelled'],
+			})
 
-            if not existing_todo:
-                assign_to_user({
-                    'doctype': self.doctype,
-                    'name': self.name,
-                    'assign_to': [user],
-                    'description': f'You have been assigned a ticket by  team {self.agent_group}',
-                })
+			if not existing_todo:
+				assign_to_user({
+					'doctype': self.doctype,
+					'name': self.name,
+					'assign_to': [user],
+					'description': f'You have been assigned a ticket by  team {self.agent_group}',
+				})
 
-    def get_active_users_from_team(self, team_name):
-        '''Return active users (User IDs) from HD Team based on active HD Agent mapping.'''
+	def get_active_users_from_team(self, team_name):
+		'''Return active users (User IDs) from HD Team based on active HD Agent mapping.'''
 
-        if not frappe.db.exists('HD Team', team_name):
-            return []
+		if not frappe.db.exists('HD Team', team_name):
+			return []
 
-        team = frappe.get_doc('HD Team', team_name)
-        team_users = [row.user for row in team.users if row.user]
+		team = frappe.get_doc('HD Team', team_name)
+		team_users = [row.user for row in team.users if row.user]
 
-        if not team_users:
-            return []
+		if not team_users:
+			return []
 
-        # Fetch user + is_active for each agent
-        agents = frappe.get_all(
-            'HD Agent',
-            filters={'user': ['in', team_users]},
-            fields=['user', 'is_active']
-        )
-        # Return only the active ones
-        active_users = [agent.user for agent in agents if agent.is_active == 1]
-        return active_users
+		# Fetch user + is_active for each agent
+		agents = frappe.get_all(
+			'HD Agent',
+			filters={'user': ['in', team_users]},
+			fields=['user', 'is_active']
+		)
+		# Return only the active ones
+		active_users = [agent.user for agent in agents if agent.is_active == 1]
+		return active_users
 
 
-    def set_agent_group(self):
-        '''Set agent_group directly from HD Team or fallback to default team.'''
+	def set_agent_group(self):
+		'''Set agent_group directly from HD Team or fallback to default team.'''
 
-        if self.agent_group:
-            return
+		if self.agent_group:
+			return
 
-        # get default team from Beams HR Settings
-        default_team = frappe.db.get_single_value('Beams Admin Settings', 'default_hd_ticket_team')
+		# get default team from Beams HR Settings
+		default_team = frappe.db.get_single_value('Beams Admin Settings', 'default_hd_ticket_team')
 
-        if default_team:
-            self.agent_group = default_team
-        else:
-            self.agent_group = ''
-            
-            
-            
+		if default_team:
+			self.agent_group = default_team
+		else:
+			self.agent_group = ''
+
 
 @frappe.whitelist()
 def create_material_request_from_ticket(ticket_name):
-    ticket = frappe.get_doc("HD Ticket", ticket_name)
-    mr = frappe.new_doc("Material Request")
-    mr.material_request_type = "Purchase"
-    mr.transaction_date = frappe.utils.today()
+	"""
+		Create Material Request from HD Ticket
+	"""
+	ticket = frappe.get_doc("HD Ticket", ticket_name)
+	mr = frappe.new_doc("Material Request")
+	mr.material_request_type = "Purchase"
+	mr.transaction_date = frappe.utils.today()
 
-    for row in ticket.material_request_items:
-        mr.append("items", {
-            "item_code": row.item,
-            "qty": row.quantity,
-            "uom": frappe.db.get_value("Item", row.item, "stock_uom"),
-            "schedule_date": row.required_by or frappe.utils.today()
-        })
+	for row in ticket.material_request_items:
+		mr.append("items", {
+			"item_code": row.item,
+			"qty": row.quantity,
+			"uom": frappe.db.get_value("Item", row.item, "stock_uom"),
+			"schedule_date": row.required_by or frappe.utils.today()
+		})
+		mr.insert(ignore_permissions=True)
 
-    return mr.as_dict()
-
-
+	return mr.name
 
