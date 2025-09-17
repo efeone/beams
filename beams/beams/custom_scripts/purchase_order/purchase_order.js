04281eac56b8674b06c45cd90e0fdbbbc9f7a235
NaseeraVP <naseeravp38@gmail.com>
2025-09-17 17:14:38 +0530

    feat:Apply restrictions to workflow buttons

---
+++ beams/beams/custom_scripts/purchase_order/purchase_order.js
@@ -0,0 +1,39 @@
+frappe.ui.form.on('Purchase Order', {
+	refresh(frm) {
+		workflow_actions(frm);
+		
+	}
+});
+
+/**
+ * Shows workflow buttons to the Purchase Order form based on the total amount.
+ */
+function workflow_actions(frm) {
+	if (frm.doc.workflow_state === "Pending Accounts Approval"){;
+		const add_workflow_button = (label, action) => {
+			frm.page.add_action_item(__(label), () => {
+				frappe.xcall('frappe.model.workflow.apply_workflow', {
+					doc: frm.doc,
+					action: action
+				})
+			});
+		};
+
+		frappe.db.get_single_value('Beams Accounts Settings', 'purchase_threshold')
+			.then(threshold => {
+				if (!threshold) {
+					return;
+				}
+
+				frm.page.clear_actions_menu();
+				const grand_total = frm.doc.grand_total;
+				let actions = ['Forward to CEO'];
+				if (grand_total < threshold) {
+					actions.unshift('Approve', 'Reject');
+				}
+
+				actions.forEach(action => add_workflow_button(action, action));
+			});
+		}
+
+}
