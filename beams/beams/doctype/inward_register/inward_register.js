04c624abdda58f848c4a7f64716cffc0a0f33cc1
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-09-17 09:46:42 +0530

    feat: remove Security role restriction for Visitor Pass

--- beams/beams/doctype/inward_register/inward_register.js
+++ beams/beams/doctype/inward_register/inward_register.js
@@ -9,36 +9,34 @@ frappe.ui.form.on('Inward Register', {
 				});
 			}, __("Create"));
 
-			if (!frappe.user.has_role('Security')) {
-				frm.add_custom_button(__(' Visitor Pass'), function () {
-					frappe.new_doc("Visitor Pass", {
-						inward_register: frm.doc.name,
-						issued_date: frappe.datetime.now_date(),
-						issued_time: frappe.datetime.now_time(),
-						issued_to: frm.doc.visitor_name,
-						purpose_of_visit: frm.doc.purpose_of_visit
-					});
-				}, __("Create"));
-				if (frm.doc.visitor_type === 'Courier') {
-					frm.add_custom_button(__('Courier Log'), function () {
-						frappe.model.with_doctype("Courier Log", function () {
-							let doc = frappe.model.get_new_doc("Courier Log");
+            frm.add_custom_button(__(' Visitor Pass'), function () {
+                frappe.new_doc("Visitor Pass", {
+                    inward_register: frm.doc.name,
+                    issued_date: frappe.datetime.now_date(),
+                    issued_time: frappe.datetime.now_time(),
+                    issued_to: frm.doc.visitor_name,
+                    purpose_of_visit: frm.doc.purpose_of_visit
+                });
+            }, __("Create"));
+            if (frm.doc.visitor_type === 'Courier') {
+                frm.add_custom_button(__('Courier Log'), function () {
+                    frappe.model.with_doctype("Courier Log", function () {
+                        let doc = frappe.model.get_new_doc("Courier Log");
 
-							doc.courier_service = frm.doc.courier_service;
-							doc.date = frm.doc.visit_date;
-							doc.status = "Received";
+                        doc.courier_service = frm.doc.courier_service;
+                        doc.date = frm.doc.visit_date;
+                        doc.status = "Received";
 
-							let inward_row = frappe.model.add_child(doc, "inward_register");
-							inward_row.inward_register = frm.doc.name;
+                        let inward_row = frappe.model.add_child(doc, "inward_register");
+                        inward_row.inward_register = frm.doc.name;
 
-							let recipient_row = frappe.model.add_child(doc, "recipients");
-							recipient_row.recepient = frm.doc.received_by;
+                        let recipient_row = frappe.model.add_child(doc, "recipients");
+                        recipient_row.recepient = frm.doc.received_by;
 
-							frappe.set_route("Form", "Courier Log", doc.name);
-						});
-					}, __("Create"));
-				}
-			}
+                        frappe.set_route("Form", "Courier Log", doc.name);
+                    });
+                }, __("Create"));
+            }
 		}
 	},
 	posting_date: function (frm) {
