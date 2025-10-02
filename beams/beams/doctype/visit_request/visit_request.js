c9cd67c6c20b0c1ecd9ecf3c00e9034071cdd636
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-10-02 14:53:27 +0530

    feat: add custom 'Inward Register' button for Visit Request when workflow_state is Security Informed

--- beams/beams/doctype/visit_request/visit_request.js
+++ beams/beams/doctype/visit_request/visit_request.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Visit Request", {
-// 	refresh(frm) {
+frappe.ui.form.on("Visit Request", {
+    refresh(frm) {
+        create_inward_register(frm);
+    },
+});
 
-// 	},
-// });
+function create_inward_register(frm) {
+    if (frm.doc.workflow_state === "Security Informed") {
+        frm.add_custom_button("Inward Register", ()=> {
+            frappe.new_doc("Inward Register", {
+                visit_request       : frm.doc.name,
+                visitor_type        : frm.doc.visitor_type,
+                visitor_name        : frm.doc.visitor_name,
+                purpose_of_visit    :frm.doc.purpose_of_visit
+            });
+    },__("Create"));
+    }
+
+}
+
