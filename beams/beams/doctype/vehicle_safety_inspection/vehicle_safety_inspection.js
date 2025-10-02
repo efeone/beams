d5e512ea4b6174cbbabe456ae71e571d8a61b0dc
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-10-02 14:51:47 +0530

    fix: filter vehicle field to show only vehicles without a safety inspection

--- beams/beams/doctype/vehicle_safety_inspection/vehicle_safety_inspection.js
+++ beams/beams/doctype/vehicle_safety_inspection/vehicle_safety_inspection.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Vehicle Safety Inspection", {
-// 	refresh(frm) {
+frappe.ui.form.on("Vehicle Safety Inspection", {
+    refresh(frm) {
+        frm.set_query("vehicle", () => {
+            return {
+                filters: {
+                    vehicle_safety_inspection: "",
+                }
+            };
+        });
+    }
+});
 
-// 	},
-// });
