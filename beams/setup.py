fe82b52742d0231213d37e77d91700ab05634e13
anjusha <anjushasuresh85@gmail.com>
2025-10-16 16:47:32 +0530

    fix: correct typo in field lable for job applicant

--- beams/setup.py
+++ beams/setup.py
@@ -3161,7 +3161,7 @@ def get_job_applicant_custom_fields():
 			{
 				"fieldname": "specialised_training",
 				"fieldtype": "Small Text",
-				"label": "Have you attended any specialised training program?If yes, Please give detais :",
+				"label": "Have you attended any specialised training program?If yes, Please give details :",
 				"insert_after": "political_org"
 			},
 			{
