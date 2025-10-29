23ae267655cad4685f754150f18f1de4e3f28f81
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-10-29 14:34:34 +0530

    fix: Ensure each row in asset_auditing_detail has exactly 3 images attached

--- beams/beams/doctype/asset_auditing/asset_auditing.py
+++ beams/beams/doctype/asset_auditing/asset_auditing.py
@@ -7,11 +7,24 @@ from frappe.utils import today
 from frappe import _
 
 class AssetAuditing(Document):
+
     def before_save(self):
         self.validate_posting_date()
-        
+    
+    def before_submit(self):
+        self.validate_image_attached()
+    
+    def validate_image_attached(self):
+        """ 
+        Ensure each row in asset_auditing_detail has exactly 3 images attached.
+        """
+        for idx, row in enumerate(self.asset_auditing_detail, start=1):
+            images = [row.image, row.image_2, row.image_3]
+            attached_images = [img for img in images if img]
+            if len(attached_images) != 3:
+                frappe.throw(f"Row {idx} must have exactly 3 images attached.")
+    
     @frappe.whitelist()
     def validate_posting_date(self):
-        if self.posting_date:
-            if self.posting_date > today():
-                frappe.throw(_("Posting Date cannot be set after today's date."))
+        if self.posting_date and self.posting_date > today():
+            frappe.throw(_("Posting Date cannot be set after today's date."))
