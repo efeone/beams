77157690caa17363d79b13d6d03feee44d9c5712
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-11-14 17:40:12 +0530

    fix: chore remove active agents filter

--- beams/beams/custom_scripts/hd_ticket/hd_ticket.js
+++ beams/beams/custom_scripts/hd_ticket/hd_ticket.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on('HD Ticket', {
-    // Called when form is loaded
+
     onload(frm) {
         if (frm.is_new() && !frm.doc.requested_employee) {
             frappe.db.get_value("Employee", { "user_id": frappe.session.user }, "name")
@@ -16,19 +16,18 @@ frappe.ui.form.on('HD Ticket', {
     },
 
     refresh(frm) {
+
         frappe.call({
             method: 'frappe.client.get_value',
             args: {
                 doctype: 'HD Agent',
                 fieldname: 'name',
                 filters: {
-                    user: frappe.session.user,
-                    is_active: 1
+                    user: frappe.session.user
                 }
             },
             callback: function(r) {
                 if (r.message && r.message.name) {
-                    // User is an active HD Agent, show all buttons
                     working_button(frm);
                     transfer_ticket(frm);
                     resolved_button(frm);
@@ -46,7 +45,6 @@ frappe.ui.form.on('HD Ticket', {
             .then(r => frm.set_value('agent_group', r.message?.team_name || ''))
             .catch(() => frm.set_value('agent_group', ''));
     },
-    
 });
 
 
@@ -55,6 +53,7 @@ frappe.ui.form.on('HD Ticket', {
  */
 function working_button(frm) {
     if (['Open', 'Transferred'].includes(frm.doc.status)) {
+
         const btn = frm.add_custom_button(__('Working'), () => {
             frappe.call({
                 method: "beams.beams.custom_scripts.hd_ticket.hd_ticket.assign_to_current_user",
@@ -68,7 +67,6 @@ function working_button(frm) {
                             message: __('Ticket assigned to you'),
                             indicator: 'green'
                         });
-
                         frm.reload_doc();
                     }
                 }
@@ -87,11 +85,13 @@ function working_button(frm) {
 
 
 /**
- * Open dialog to transfer ticket to another active agent
+ * Open dialog to transfer ticket to any HD Agent
  */
 function transfer_ticket(frm) {
     if (!['Closed', 'Open'].includes(frm.doc.status)) {
+
         const btn = frm.add_custom_button(__('Transfer'), () => {
+
             let d = new frappe.ui.Dialog({
                 title: 'Transfer Ticket',
                 fields: [
@@ -99,14 +99,12 @@ function transfer_ticket(frm) {
                         label: 'Agent',
                         fieldname: 'agent',
                         fieldtype: 'Link',
-                        options: 'HD Agent',
-                        get_query: function() {
-                            return { filters: { 'is_active': 1 } };
-                        }
+                        options: 'HD Agent'
                     }
                 ],
                 size: 'small',
                 primary_action_label: 'Assign',
+
                 primary_action(values) {
                     frappe.call({
                         method: 'beams.beams.custom_scripts.hd_ticket.hd_ticket.assign_ticket_to_agent',
@@ -116,7 +114,10 @@ function transfer_ticket(frm) {
                         },
                         callback: function(r) {
                             if (!r.exc) {
-                                frappe.show_alert({ message: __('Ticket transferred successfully'), indicator: 'orange' });
+                                frappe.show_alert({
+                                    message: __('Ticket transferred successfully'),
+                                    indicator: 'orange'
+                                });
                                 frm.set_value('status', 'Transferred');
                                 frm.save().then(() => frm.reload_doc());
                             }
@@ -144,8 +145,8 @@ function transfer_ticket(frm) {
  */
 function resolved_button(frm) {
     if (!['Closed', 'Open'].includes(frm.doc.status)) {
-        const btn = frm.add_custom_button(__('Resolved'), () => {
 
+        const btn = frm.add_custom_button(__('Resolved'), () => {
             frm.set_value('status', 'Closed');
             frm.save().then(() => {
                 frappe.show_alert({ message: __('Ticket marked as closed'), indicator: 'green' });
@@ -163,31 +164,29 @@ function resolved_button(frm) {
 }
 
 
-
 /**
- * Add Asset Request and Material Request buttons if user is an active HD Agent
+ * Add Asset Request + Material Request Buttons
  */
 function add_request_buttons(frm) {
+
     frappe.call({
         method: 'frappe.client.get_value',
         args: {
             doctype: 'HD Agent',
             fieldname: 'name',
             filters: {
-                user: frappe.session.user,
-                is_active: 1
+                user: frappe.session.user
             }
         },
         callback: function(r) {
             if (r.message && r.message.name) {
-                // Asset Request button
+
                 frm.add_custom_button(__('Asset Request'), () => {
                     frappe.new_doc('Asset Request', {
                         'requested_by': frm.doc.requested_employee
                     });
                 }, __('Create'));
 
-                // Material Request button
                 frm.add_custom_button(__('Material Request'), () => {
                     frappe.new_doc('Material Request', {
                         'requested_by': frm.doc.requested_employee
