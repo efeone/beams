2140afb153a55bff5824330ba6c149d36419a375
anjusha <anjushasuresh85@gmail.com>
2025-11-21 10:26:59 +0530

    fix: resolve migration failure

--- beams/patches/delete_property_setter.py
+++ beams/patches/delete_property_setter.py
@@ -1,18 +1,27 @@
 import frappe
 
 def execute():
-    # Define the filter to find the Property Setter
-    filters = {
-        "doctype_or_field": "DocField",
-        "doc_type": "Employee Feedback Rating",
-        "field_name": "Rating",
-        "property": "read_only",
-        "property_type": "Check",
-        "value": "1"  
-    }
+	filters_list = [
+		{
+			"doctype_or_field": "DocField",
+			"doc_type": "Employee Feedback Rating",
+			"field_name": "Rating",
+			"property": "read_only",
+			"property_type": "Check",
+			"value": "1"
+		},
+		{
+			"doctype_or_field": "DocField",
+			"doc_type": "Employee Advance",
+			"field_name": "purpose",
+			"property": "hidden",
+			"property_type": "Small Text",
+			"value":1
+		},
+	]
 
-    # Find and delete the property setter if it exists
-    property_setter_name = frappe.db.exists("Property Setter", filters)
-    if property_setter_name:
-        frappe.db.delete("Property Setter", {"name": property_setter_name})
-        frappe.db.commit()
+	for filters in filters_list:
+		property_setter_name = frappe.db.exists("Property Setter", filters)
+		if property_setter_name:
+			frappe.db.delete("Property Setter", {"name": property_setter_name})
+	frappe.db.commit()
