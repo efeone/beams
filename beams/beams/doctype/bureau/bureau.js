87a441f8fe246dd9122251595cdf75971fb4138f
Avishna Murali <avishnachinnu26@gmail.com>
2025-12-01 14:58:49 +0530

    feat:Show Bureaus marked as Is Parent Bureau = 1 appear in the selection list

--- beams/beams/doctype/bureau/bureau.js
+++ beams/beams/doctype/bureau/bureau.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bureau", {
-// 	refresh(frm) {
+frappe.ui.form.on("Bureau", {
+	setup(frm) {
+		set_filters(frm);
+	}
+});
 
-// 	},
-// });
+/*
+Show Bureaus marked as "Is Parent Bureau = 1" appear in the selection list.
+*/
+let set_filters = function (frm) {
+	frm.set_query("regional_bureau", function() {
+		return {
+			filters: {
+				is_parent_bureau: 1
+			}
+		}
+	});
+}
