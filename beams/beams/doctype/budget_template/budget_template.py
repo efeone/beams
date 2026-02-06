eaeea1e2a1af826f005f5ac126e156a15a40bcc1
ajmalroshan123 <ajmalroshan2002@gmail.com>
2026-02-06 10:57:47 +0530

    feat: prevent budget template duplicate with cost center

--- beams/beams/doctype/budget_template/budget_template.py
+++ beams/beams/doctype/budget_template/budget_template.py
@@ -3,8 +3,12 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class BudgetTemplate(Document):
+
+	def validate(self):
+		self.validate_account_per_cost_center()
 
 	def set_default_account(self):
 		"""  Set the default account for each budget template item based on the associated cost subhead and company.
@@ -28,6 +32,54 @@ class BudgetTemplate(Document):
 	def before_save(self):
 		self.set_default_account()
 
+
+	def validate_account_per_cost_center(self):
+		if not self.budget_template_items and self.cost_center:
+			return
+
+		for row in self.budget_template_items or []:
+			if not row.account_head:
+				continue
+
+			duplicates = frappe.get_all(
+				"Budget Template Item",
+				filters={
+					"account_head": row.account_head,
+					"parenttype": "Budget Template",
+					"parent": ["!=", self.name],
+				},
+				fields=["parent"],
+				limit=1,
+			)
+
+			if not duplicates:
+				continue
+
+			template = duplicates[0].parent
+
+			# check cost center of that template
+			cost_center = frappe.db.get_value(
+				"Budget Template", template, "cost_center"
+			)
+
+			if cost_center != self.cost_center:
+				continue
+
+			template_link = frappe.utils.get_link_to_form(
+				"Budget Template", template
+			)
+
+			frappe.throw(
+				_(
+					"Account : <b>{0}</b> is used in the {1} Budget Template "
+					"with the same Cost Center : <b>{2}</b>."
+				).format(
+					row.account_head,
+					template_link,
+					self.cost_center,
+				),
+				title=_("Duplicate Account Found"),
+			)
 
 
 @frappe.whitelist()
