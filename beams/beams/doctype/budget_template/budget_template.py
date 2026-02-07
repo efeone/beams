7050d5e5bacc914014f1143a827ce280b19c9435
Sherin KR <sherinkrply@gmail.com>
2026-02-07 14:12:59 +0530

    feat: applied filter for region and budget head

--- beams/beams/doctype/budget_template/budget_template.py
+++ beams/beams/doctype/budget_template/budget_template.py
@@ -5,17 +5,16 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 
-
 class BudgetTemplate(Document):
 
 	def validate(self):
 		self.validate_account_per_cost_center()
 
 	def validate_account_per_cost_center(self):
-		"""
+		'''
 			Validates that there are no duplicate Cost Heads within the same Budget Template and
 			no duplicate Account Heads across different Budget Templates for the same Cost Center.
-		"""
+		'''
 
 		if not self.cost_center or not self.budget_template_items:
 			return
@@ -27,9 +26,9 @@ class BudgetTemplate(Document):
 			if row.cost_head:
 				if row.cost_head in seen_cost_heads:
 					frappe.throw(
-						_("Duplicate Cost Head <b>{0}</b> is not allowed in the same Budget Template.")
+						_('Duplicate Cost Head <b>{0}</b> is not allowed in the same Budget Template.')
 						.format(row.cost_head),
-						title=_("Duplicate Cost Head"),
+						title=_('Duplicate Cost Head'),
 					)
 				seen_cost_heads.add(row.cost_head)
 
@@ -38,14 +37,14 @@ class BudgetTemplate(Document):
 				continue
 
 			duplicates = frappe.get_all(
-				"Budget Template Item",
+				'Budget Template Item',
 				filters={
-					"account_head": row.account_head,
-					"parenttype": "Budget Template",
-					"parentfield": "budget_template_items",
-					"parent": ["!=", self.name],
+					'account_head': row.account_head,
+					'parenttype': 'Budget Template',
+					'parentfield': 'budget_template_items',
+					'parent': ['!=', self.name],
 				},
-				fields=["parent"],
+				fields=['parent'],
 				limit=1,
 			)
 
@@ -55,71 +54,76 @@ class BudgetTemplate(Document):
 			template = duplicates[0].parent
 
 			template_cost_center = frappe.db.get_value(
-				"Budget Template", template, "cost_center"
+				'Budget Template', template, 'cost_center'
 			)
 
 			if template_cost_center != self.cost_center:
 				continue
 
 			template_link = frappe.utils.get_link_to_form(
-				"Budget Template", template
+				'Budget Template', template
 			)
 
 			frappe.throw(
 				_(
-					"Account : <b>{0}</b> is used in the {1} Budget Template "
-					"with the same Cost Center : <b>{2}</b>."
+					'Account : <b>{0}</b> is used in the {1} Budget Template '
+					'with the same Cost Center : <b>{2}</b>.'
 				).format(
 					row.account_head,
 					template_link,
 					self.cost_center,
 				),
-				title=_("Duplicate Account Found"),
+				title=_('Duplicate Account Found'),
 			)
-
 
 @frappe.whitelist()
 def get_budget_approver_employees(doctype, txt, searchfield, start, page_len, filters):
-    """
-    Fetch active employees for a given company & department
-    whose user has the role 'Budget Approver'.
-    """
+	'''
+		Fetch active employees for a given company & department
+		whose user has the role `Budget Approver`.
+	'''
+	if not filters:
+		return []
 
-    if not filters:
-        return []
+	company = filters.get('company')
+	department = filters.get('department')
 
-    company = filters.get("company")
-    department = filters.get("department")
+	if not company or not department:
+		return []
 
-    if not company or not department:
-        return []
+	role_name = 'Budget Approver'
+	query = '''
+		SELECT
+			emp.name,
+			emp.employee_name
+		FROM
+			`tabEmployee` emp
+			INNER JOIN `tabUser` u
+				ON u.name = emp.user_id
+			INNER JOIN `tabHas Role` hr
+				ON hr.parent = u.name
+		WHERE
+			hr.role = %(role)s
+			AND emp.status = 'Active'
+			AND (
+				emp.name LIKE %(search_txt)s
+				OR emp.employee_name LIKE %(search_txt)s
+			)
+			AND emp.company = %(company)s
+			AND emp.department = %(department)s
+		LIMIT %(start)s, %(page_len)s
+	'''
 
-    # Users with 'Budget Approver' role
-    budget_approver_users = frappe.get_all(
-        "Has Role",
-        filters={"role": "Budget Approver"},
-        pluck="parent",
-    )
+	# Prepare query parameters
+	params = {
+		'role': role_name,
+		'search_txt': f'%{txt}%',
+		'start': start,
+		'page_len': page_len,
+		'company': company,
+		'department': department
+	}
 
-    if not budget_approver_users:
-        return []
-
-    employee_filters = {
-        "user_id": ["in", budget_approver_users],
-        "company": company,
-        "department": department,
-        "status": "Active",
-    }
-
-    
-    employees = frappe.get_all(
-            "Employee",
-            filters=employee_filters,
-            fields=["name", "employee_name"],
-            limit_start=int(start or 0),
-            limit_page_length=int(page_len or 20),
-            order_by="employee_name",
-        )
-
-    return [(emp.name, emp.employee_name) for emp in employees]
-
+	# Execute query
+	budget_heads = frappe.db.sql(query, params, as_list=True)
+	return budget_heads
