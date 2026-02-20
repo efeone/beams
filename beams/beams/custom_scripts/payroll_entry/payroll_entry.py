4e230b3c665918daf6ff0b67ce78bf6015cb7d65
Jumana-K <jumanahaseen555@gmail.com>
2026-02-20 09:56:35 +0530

    fix:start date not able to change manually

--- beams/beams/custom_scripts/payroll_entry/payroll_entry.py
+++ beams/beams/custom_scripts/payroll_entry/payroll_entry.py
@@ -2,19 +2,12 @@ import frappe
 from frappe.utils import add_months, get_first_day, get_last_day, getdate
 
 def set_previous_month_dates(doc, method=None):
-    """Auto-set start_date and end_date to PREVIOUS month based on posting_date"""
-    
-    if not doc.posting_date:
-        return
-    
-    # Convert posting_date to date object
-    posting_date = getdate(doc.posting_date)
-    
-    # Get PREVIOUS month's first and last day
-    prev_month_date = add_months(posting_date, -1)
-    start_date = get_first_day(prev_month_date)
-    end_date = get_last_day(prev_month_date)
-    
-    # Set the dates to previous month
-    doc.start_date = start_date
-    doc.end_date = end_date 
+	"""Auto-set start_date and end_date to PREVIOUS month based on posting_date"""
+	if doc.posting_date and doc.payroll_frequency == 'Monthly':
+		if not doc.start_date or not doc.end_date:
+			# Get the first day of the previous month
+			posting_date = getdate(doc.posting_date)
+			prev_month_date = add_months(posting_date, -1)
+
+			doc.start_date = get_first_day(prev_month_date)
+			doc.end_date = get_last_day(prev_month_date)
